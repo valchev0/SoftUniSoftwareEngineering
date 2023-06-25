c30582e9d3e3ad126472d74294732fd6f245df83
valchev0 <georgi.v@mail.com>
2023-06-25 11:30:01 +0100

    ImplementAlgorithmForEachNumber

---
+++ CSharpBasics/CSharpBasics2015/MathForDevelopers/ImplementAlgorithmForEachNumber/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class ImplementAlgorithmForEachNumber
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string container = string.Empty;
+        bool isDone = false;
+        int currentNum = 0;
+        while (true)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                currentNum = input[i];
+                int digit = 0;
+                for (int j = 0; j < 10; j++)
+                {
+                    digit = currentNum % 2;
+                    currentNum /= 2;
+                    container += digit;
+                    if (currentNum <=0)
+                    {
+                        if (container.Length % 2 != 0)
+                        {
+                            container += 0;
+                        }
+                        if (container.Length % 4 != 0)
+                        {
+                            container += 0;
+                        }
+                        if (container.Length % 8 != 0)
+                        {
+                            container += 0;
+                        }
+                        char[] charArray = container.ToCharArray();
+                        Array.Reverse(charArray);
+                        string binary = new string(charArray);
+                        Console.Write(binary + " ");
+                        container = string.Empty;
+                        break;
+                    }
+                }
+                if (i == input.Length - 1)
+                {
+                    isDone = true;
+                    break;
+                }
+                continue;
+            }
+            if (isDone)
+            {
+                break;
+            }
+        }
+    }
+}
