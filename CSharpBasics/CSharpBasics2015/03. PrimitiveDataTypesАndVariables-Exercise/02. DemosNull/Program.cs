8e5fcd508e76f4c05346f0e9e565dcb6969483c0
valchev0 <georgi.v@mail.com>
2023-06-25 08:32:39 +0100

    PrimitiveDataTypesАndVariables-Exercise

---
+++ CSharpBasics/CSharpBasics2015/03. PrimitiveDataTypesАndVariables-Exercise/02. DemosNull/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class DemosNull
+{
+    static void Main()
+    {
+        int testNumber = 2;
+        int? testNumberNull = null;
+        string nul = null;
+        Console.WriteLine("{0}\n {1}\n {2}\n", testNumber, testNumberNull, nul);
+    }
+}
