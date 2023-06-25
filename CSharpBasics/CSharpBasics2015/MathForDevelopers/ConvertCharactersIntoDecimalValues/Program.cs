ae88eb3e58dce845787d4c636bfe8705580e7783
valchev0 <georgi.v@mail.com>
2023-06-25 09:06:26 +0100

    MathForDevelopers

---
+++ CSharpBasics/CSharpBasics2015/MathForDevelopers/ConvertCharactersIntoDecimalValues/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ConvertCharactersIntoDecimalValues
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        for (int i = 0; i < name.Length; i++)
+        {
+            char ch = name[i];
+            int n = ch;
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ CSharpBasics/CSharpBasics2015/MathForDevelopers/ConvertingDecimalToBinary/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class ConvertingDecimalToBinary
+{
+    static void Main()
+    {
+        
+    }
+}
