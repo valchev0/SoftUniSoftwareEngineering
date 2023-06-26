1138ed82a77865b3e70a93337ad536f7e05c6330
valchev0 <georgi.v@mail.com>
2023-06-26 16:12:53 +0100

    FloatOrDouble

---
+++ CSharpBasics/CSharpBasics/PrimitiveDataTypesAndVariables-Homework/FloatOrDouble/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class DeclareVariables
+{
+    static void Main()
+    {
+        float first = 12.345f;
+        float second = 3456.091f;
+        double third = 8923.1234857;
+        double forth = 34.567839023;
+        Console.WriteLine(" {0}\n {1}\n {2}\n {3}\n", first, second, third, forth);
+    }
+}
