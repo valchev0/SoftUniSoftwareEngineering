79aad68b1c31495a07d5d5278317dbe35b3345b1
valchev0 <georgi.v@mail.com>
2023-06-26 17:24:49 +0100

    StringsAndObjects

---
+++ CSharpBasics/CSharpBasics/PrimitiveDataTypesAndVariables-Homework/StringsAndObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class StringsAndObjects
+{
+    static void Main()
+    {
+        string hello = "Hello";
+        string world = "World";
+        object concatenation = hello + " " +  world;
+        Console.WriteLine(concatenation);
+    }
+}
