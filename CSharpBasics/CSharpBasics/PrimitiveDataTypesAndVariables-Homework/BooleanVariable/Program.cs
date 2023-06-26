495cc95db86451671b023dc37daeb2a03db6208c
valchev0 <georgi.v@mail.com>
2023-06-26 16:44:03 +0100

    BooleanVariable

---
+++ CSharpBasics/CSharpBasics/PrimitiveDataTypesAndVariables-Homework/BooleanVariable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class BooleanVariable
+{
+    static void Main()
+    {
+        bool isFemale = false; // f or m
+        string gender = Console.ReadLine().ToLower();
+        if (gender == "f")
+        {
+            isFemale = true;
+        }
+        Console.WriteLine(isFemale);
+    }
+}
