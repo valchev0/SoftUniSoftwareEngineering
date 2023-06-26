76fc2bcd38de026dda124c1087540f8ffc8b9030
valchev0 <georgi.v@mail.com>
2023-06-26 19:14:21 +0100

    ExchangeVariableValues

---
+++ CSharpBasics/CSharpBasics/PrimitiveDataTypesAndVariables-Homework/ExchangeVariableValues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ExchangeVariableValues
+{
+    static void Main()
+    {
+        Console.WriteLine("Befor:");
+        var a = 5;
+        Console.WriteLine("a = 5");
+        var b = 10;
+        Console.WriteLine("b = 10");
+        b = b - a;
+        a = b + a;
+        Console.WriteLine("After:");
+        Console.WriteLine("a = 10");
+        Console.WriteLine("b = 5");
+    }
+}
