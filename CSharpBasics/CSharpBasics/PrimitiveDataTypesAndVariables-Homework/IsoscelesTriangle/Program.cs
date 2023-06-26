ed976c0ec6c01cd424a87e024fd3c1161bd24cd0
valchev0 <georgi.v@mail.com>
2023-06-26 19:07:05 +0100

    IsoscelesTriangle

---
+++ CSharpBasics/CSharpBasics/PrimitiveDataTypesAndVariables-Homework/IsoscelesTriangle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class IsoscelesTriangle
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("   ©");
+        Console.WriteLine("  © ©");
+        Console.WriteLine(" ©   ©");
+        Console.WriteLine("© © © ©");
+    }
+}
