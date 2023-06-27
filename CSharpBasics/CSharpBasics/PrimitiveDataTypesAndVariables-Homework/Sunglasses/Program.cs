c780897f3076f8d52babea97a72a4dc825b74d52
valchev0 <georgi.v@mail.com>
2023-06-27 22:03:48 +0100

    Sunglasses

---
+++ CSharpBasics/CSharpBasics/PrimitiveDataTypesAndVariables-Homework/Sunglasses/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Sunglasses
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int row = 0; row < n; row++)
+        {
+            for (int leftFrame = 0; leftFrame < 2 * n; leftFrame++)
+            {
+                if (row == 0 || row == n - 1)
+                {
+                    Console.Write("*");
+                }
+                //int spaces = ((2 * n) - 1) - row - 1;
+                else if (leftFrame == 0 || leftFrame == ((2 * n) - 1))
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("/");
+                }
+            }
+            for (int bridge = 0; bridge < n; bridge++)
+            {
+                if (row == n/2)
+                {
+                    Console.Write("|");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            for (int rightFrame = 0; rightFrame < 2 * n; rightFrame++)
+            {
+                if (row == 0 || row == n - 1)
+                {
+                    Console.Write("*");
+                }
+                //int spaces = ((2 * n) - 1) - row - 1;
+                else if (rightFrame == 0 || rightFrame == ((2 * n) - 1))
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("/");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
