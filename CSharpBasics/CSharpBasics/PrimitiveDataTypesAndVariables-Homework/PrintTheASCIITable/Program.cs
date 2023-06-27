87c2226846a684353a1a220eccd5ed0924308cfe
valchev0 <georgi.v@mail.com>
2023-06-27 04:30:57 +0100

    PrintTheASCIITable

---
+++ CSharpBasics/CSharpBasics/PrimitiveDataTypesAndVariables-Homework/PrintTheASCIITable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class PrintTheASCIITable
+{
+    static void Main()
+    {
+        for (int i = 0; i <= 255; i++)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write((char)i + " ");
+        }
+    }
+}
