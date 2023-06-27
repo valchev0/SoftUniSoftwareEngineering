8e99d44f2ed9dbcdef41b7a5627a6edbfb7fddbd
valchev0 <georgi.v@mail.com>
2023-06-27 18:16:11 +0100

    JoroTheFootballPlayer

--- CSharpBasics/CSharpBasics/PrimitiveDataTypesAndVariables-Homework/JoroTheFootballPlayer/Program.cs
+++ CSharpBasics/CSharpBasics/PrimitiveDataTypesAndVariables-Homework/JoroTheFootballPlayer/Program.cs
@@ -1,23 +1,22 @@
 ﻿using System;
+using System.Diagnostics.SymbolStore;
 
 class JoroTheFootballPlayer
 {
     static void Main()
     {
-        char year = char.Parse(Console.ReadLine());
-        bool isLeapYear = year == 't';
-        int normalHolidays = int.Parse(Console.ReadLine()); // 1/2
-        int weekendsInHome = int.Parse(Console.ReadLine());
-        int weekendsPerYear = 52;                              // only onse per weekends if he's not in his hometown 
-                                                           // and he's not tired
-        double normalWeekends = weekendsPerYear - weekendsInHome;
-        normalWeekends /= 3;
-        double playCounter = 0;
+       double weekendsPerYear = 52;
+        char yearType = char.Parse(Console.ReadLine());
+        bool isLeapYear = yearType == 't';
+        double holidaysNotWeekends = double.Parse(Console.ReadLine())/2;
+        double weekendsHometown = double.Parse(Console.ReadLine());
+        double playsWeekend = weekendsHometown + ((weekendsPerYear - weekendsHometown) * 2/3);
+        double totalPlays = 0;
         if (isLeapYear)
         {
-            playCounter = 3;
+            totalPlays += 3;
         }
-        playCounter += normalHolidays + normalWeekends;
-        Console.WriteLine(playCounter);
+        totalPlays += holidaysNotWeekends + playsWeekend;
+        Console.WriteLine(Math.Floor(totalPlays));
     }
 }
