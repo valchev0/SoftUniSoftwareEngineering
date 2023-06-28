7bf7b3b72337bd5fe54599e6b950cf6c56538e20
valchev0 <georgi.v@mail.com>
2023-06-28 22:54:53 +0100

    Demos Training

--- CSharpBasics/CSharpBasicsOld/ConsoleInputAndOutput/01. DemosFloat/Program.cs
+++ CSharpBasics/CSharpBasicsOld/ConsoleInputAndOutput/01. DemosFloat/Program.cs
@@ -7,6 +7,8 @@ namespace MyConsoleApp
         {
             float num = 541.123f;
             Console.WriteLine("{0:00.00}", num);  //514.12
+            string formatedNum = String.Format("{0:00.00}", num);
+            Console.WriteLine("{0:00.00}", num); 
         }
     }
 }
---
+++ CSharpBasics/CSharpBasicsOld/ConsoleInputAndOutput/02. DemosReadChar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chCode = 0;
+            while (chCode != (int)ConsoleKey.Enter) 
+            {
+                chCode = Console.Read();
+
+                Console.WriteLine(chCode);
+            }
+        }
+    }
+}
