49f71600e3c4e30e7b7b6a5b60f909e6460348cc
valchev0 <georgi.v@mail.com>
2023-06-28 21:56:57 +0100

    Demos Training

---
+++ CSharpBasics/CSharpBasicsOld/ConsoleInputAndOutput/00. Demos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstColumnAlignment = -20;
+            string nameGeorge = "George";
+            int agesGeorge = 18;
+
+            string nameStan = "Stan";
+            int agesStan = 20;
+
+            Console.WriteLine("{0," + firstColumnAlignment +"}{1, 3}", "Name", "Age");
+            Console.WriteLine("{0, -20}{1, 3}", nameGeorge, agesGeorge);
+            Console.WriteLine("{0, -20}{1, 3}", nameStan, agesStan);
+        }
+    }
+}
