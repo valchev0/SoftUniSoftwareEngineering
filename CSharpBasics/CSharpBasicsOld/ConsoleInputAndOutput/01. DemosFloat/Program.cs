a6043699432b50df0f65921b92884149ff9b9986
valchev0 <georgi.v@mail.com>
2023-06-28 22:03:38 +0100

    Demos Training

---
+++ CSharpBasics/CSharpBasicsOld/ConsoleInputAndOutput/01. DemosFloat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num = 541.123f;
+            Console.WriteLine("{0:00.00}", num);  //514.12
+        }
+    }
+}
