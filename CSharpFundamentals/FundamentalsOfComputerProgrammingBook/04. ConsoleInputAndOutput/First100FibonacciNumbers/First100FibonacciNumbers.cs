8731cdd4b0dd65f83d8ea0fabe2a992eb3b8f511
valchev0 <georgi.v@mail.com>
2023-06-29 19:39:46 +0100

    First100FibonacciNumbers

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/04. ConsoleInputAndOutput/First100FibonacciNumbers/First100FibonacciNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace First100FibonacciNumbers
+{
+    internal class First100FibonacciNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int previousNumber = 0;
+            int fibonacciCounter = 0;
+
+            while(fibonacciCounter < 100)
+            {
+                n += previousNumber;
+                Console.WriteLine(previousNumber);
+                previousNumber = n - previousNumber;
+                fibonacciCounter++;
+            }
+        }
+    }
+}
