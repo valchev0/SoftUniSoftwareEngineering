b446484e765d2cb644dcd4b3ea83e20925efd7af
valchev0 <georgi.v@mail.com>
2023-06-29 17:45:20 +0100

    PrintsThreeNumbers

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/4.1 ConsoleInputAndOutput/PrintsThreeNumbers/PrintsThreeNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace PrintsThreeNumbers
+{
+    internal class PrintsThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            string hexString = Console.ReadLine();
+            int firsNum = int.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
+            double secondNum = double.Parse(Console.ReadLine());
+            double thirdNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0, -10}{1:0.00, 10}{2:0.00, 20}", firsNum, secondNum, thirdNum);
+        }
+    }
+}
