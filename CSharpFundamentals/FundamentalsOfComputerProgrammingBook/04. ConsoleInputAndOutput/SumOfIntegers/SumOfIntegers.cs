28471f97212bed7d4df7fe769ace51d94696d76a
valchev0 <georgi.v@mail.com>
2023-06-29 19:30:16 +0100

    SumOfIntegers

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/04. ConsoleInputAndOutput/SumOfIntegers/SumOfIntegers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SumOfIntegers
+{
+    internal class SumOfIntegers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
