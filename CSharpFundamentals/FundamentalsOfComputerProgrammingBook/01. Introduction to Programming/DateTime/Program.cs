96addba586d05f9013f2621f8fe83e9aa0329d10
valchev0 <georgi.v@mail.com>
2023-06-29 16:30:43 +0100

    Introduction to Programming

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/01. Introduction to Programming/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+class GFG
+{
+
+    // Main Method
+    public static void Main()
+    {
+
+        DateTime? a = null;
+        if (!a.HasValue)
+        {
+            a = DateTime.Now;
+            if (a.HasValue)
+            {
+                Console.WriteLine(a.Value);
+            }
+        }
+
+    }
+}
---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/01. Introduction to Programming/DateTimeAddYears/Program.cs
@@ -0,0 +1,40 @@
+﻿// C# program to demonstrate the
+// DateTime.AddYears(Int32) Method
+using System;
+using System.Globalization;
+
+class GFG
+{
+
+    // Main Method
+    public static void Main()
+    {
+
+        try
+        {
+
+            // creating object of DateTime
+            DateTime date1 = new DateTime(2023, 6, 14,
+                                            4, 0, 15);
+
+            // adding the 8 Months
+            // using AddYears() method;
+            DateTime date2 = date1.AddYears(8);
+
+            // Display the date1
+            Console.WriteLine("DateTime before operation: "
+                                  + "{0:y} {0:dd}", date1);
+
+            // Display the date2
+            Console.WriteLine("\nDateTime after operation: "
+                                  + "{0:y} {0:dd}", date2);
+
+        }
+
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.Write("Exception Thrown: ");
+            Console.Write("{0}", e.GetType(), e.Message);
+        }
+    }
+}
