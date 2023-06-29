d1a6e2c59c1584dcef3003573ae6018ac7e53f99
valchev0 <georgi.v@mail.com>
2023-06-29 16:14:40 +0100

    DemosTryCatch

---
+++ CSharpBasics/CSharpBasicsOld/ConsoleInputAndOutput/03. DemosTryCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+
+            Console.WriteLine("Input {0}'s age: ", age);
+            string strAge = Console.ReadLine();
+
+            try
+            {
+                age = Int32.Parse(strAge);
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Wrong data format!");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
