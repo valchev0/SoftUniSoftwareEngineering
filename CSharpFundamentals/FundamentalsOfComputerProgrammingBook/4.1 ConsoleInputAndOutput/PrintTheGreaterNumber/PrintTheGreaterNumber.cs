efca3c3b858d567969269370b6a50baf7713775b
valchev0 <georgi.v@mail.com>
2023-06-29 17:53:31 +0100

    PrintTheGreaterNumber

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/4.1 ConsoleInputAndOutput/PrintTheGreaterNumber/PrintTheGreaterNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PrintTheGreaterNumber
+{
+    internal class PrintTheGreaterNumber
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Max(firstNum, secondNum));
+        }
+    }
+}
