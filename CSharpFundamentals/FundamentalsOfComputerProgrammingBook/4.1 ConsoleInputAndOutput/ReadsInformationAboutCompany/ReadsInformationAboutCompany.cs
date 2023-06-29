e0b7f36cf99e4d62acfcbc38fe11cfdd2dd09ec8
valchev0 <georgi.v@mail.com>
2023-06-29 17:34:11 +0100

    ReadsInformationAboutCompany

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/4.1 ConsoleInputAndOutput/ReadsInformationAboutCompany/ReadsInformationAboutCompany.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ReadsInformationAboutCompany
+{
+    internal class ReadsInformationAboutCompany
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Company name: ");
+            string companyName = Console.ReadLine();
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+            Console.Write("Phone number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Fax number: ");
+            string faxNumber = Console.ReadLine();
+            Console.Write("Web site: ");
+            string webSite = Console.ReadLine();
+            Console.Write("Manager's full name: ");
+            string managerName = Console.ReadLine();
+            Console.Write("His phone number: ");
+            string managersPhoneNumber = Console.ReadLine();
+        }
+    }
+}
