495fe075493baca5f9988df366ae4c7e399498f7
valchev0 <georgi.v@mail.com>
2023-06-29 16:54:41 +0100

    FindPerimeterAndArea

--- CSharpFundamentals/FundamentalsOfComputerProgrammingBook/4.1 ConsoleInputAndOutput/FindPerimeterAndArea/FindPerimeterAndArea.cs
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/4.1 ConsoleInputAndOutput/FindPerimeterAndArea/FindPerimeterAndArea.cs
@@ -5,7 +5,13 @@ namespace FindPerimeterAndArea
     {
         static void Main(string[] args)
         {
-            
+            double radius = double.Parse(Console.ReadLine());
+
+            double perimeter = 2 * Math.PI * radius;
+            Console.WriteLine("Perimeter is {0:0.00}", perimeter);
+
+            double area = Math.PI * (Math.Pow(radius, 2));
+            Console.WriteLine("Area is {0:0.00}", area);
         }
     }
 }
