c50db2ed7666d8566d218225b38678db1c9b71ef
valchev0 <georgi.v@mail.com>
2023-06-29 17:49:57 +0100

    RemainderOfTheirDivisionBy5

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/4.1 ConsoleInputAndOutput/RemainderOfTheirDivisionBy5/RemainderOfTheirDivisionBy5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace RemainderOfTheirDivisionBy5
+{
+    internal class RemainderOfTheirDivisionBy5
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
