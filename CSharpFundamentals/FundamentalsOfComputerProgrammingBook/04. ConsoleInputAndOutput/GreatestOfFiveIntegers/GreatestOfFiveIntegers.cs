361676674856ffbb4bd1a72c655f87ba1e3c0e5d
valchev0 <georgi.v@mail.com>
2023-06-29 19:22:13 +0100

    GreatestOfFiveIntegers

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/04. ConsoleInputAndOutput/GreatestOfFiveIntegers/GreatestOfFiveIntegers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace GreatestOfFiveIntegers
+{
+    internal class GreatestOfFiveIntegers
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+            int fifthNum = int.Parse(Console.ReadLine());
+            int firstGreatNum = Math.Max(firstNum, secondNum);
+            int secondGreatNum = Math.Max(firstGreatNum, thirdNum);
+            int thirdGreatNum = Math.Max(secondGreatNum, fourthNum);
+            int fifthGreatNum = Math.Max(thirdGreatNum, fifthNum);
+
+            Console.WriteLine(fifthGreatNum);
+        }
+    }
+}
