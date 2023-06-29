e306c3250f8e87e99dfb22c6267a0f808e2e6a5d
valchev0 <georgi.v@mail.com>
2023-06-29 19:13:31 +0100

    PrintSumOfFiveIntegers

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/04. ConsoleInputAndOutput/PrintSumOfFiveIntegers/PrintSumOfFiveIntegers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace PrintSumOfFiveIntegers
+{
+    internal class PrintSumOfFiveIntegers
+    {
+        static void Main(string[] args)
+        {
+			int num = 0;
+            int counter = 0;
+            int sum = 0;
+
+            while (counter < 5)
+            {
+                try
+                {
+                    num = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid format data!");
+                    continue;
+                }
+                sum += num;
+                counter++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
