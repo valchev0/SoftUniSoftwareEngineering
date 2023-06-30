07eace993960e3a716ae03527d2e2bd31732022f
valchev0 <georgi.v@mail.com>
2023-06-30 03:36:07 +0100

    DemosFloat

---
+++ CSharpBasics/CSharpBasicsOld/ConsoleInputAndOutput/05. DemosFloat/DemosFloat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace DemosFloat
+{
+    internal class DemosFloat
+    {
+        static void Main(string[] args)
+        {
+            float num = 0f;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            string str = Console.ReadLine();
+            str = str.Replace(',', '.');
+
+            num = Single.Parse(str);
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ CSharpBasics/CSharpBasicsOld/ConsoleInputAndOutput/PrintingToTheConsole/PrintingToTheConsole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace PrintingToTheConsole
+{
+    internal class PrintingToTheConsole
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            decimal c = decimal.Parse(Console.ReadLine());
+            Console.Write("{0:X}", a);
+            string secondNum = double.Parse(Console.ReadLine()).Convert.ToString(b, 2);
+            //Console.WriteLine("{0:X, -20}{1:B:00, 10}{2:0.00, 10}", a, b, c);
+            //Console.WriteLine("{0:X, -20}{1:B:00, 10}{2:0.00, 10}", a, b, c);
+            //Console.WriteLine("{0:X, -20}{1:B:00, 10}{2:0.00, 10}", a, b, c);
+        }
+    }
+}
