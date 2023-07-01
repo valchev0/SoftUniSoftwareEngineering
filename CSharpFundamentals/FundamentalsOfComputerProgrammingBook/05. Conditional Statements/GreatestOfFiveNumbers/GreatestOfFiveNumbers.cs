25900166836e80ddbba17dec5df9757d270632d8
valchev0 <georgi.v@mail.com>
2023-07-01 20:02:00 +0100

    WhatTypeWillBeTheInput

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/05. Conditional Statements/GreatestOfFiveNumbers/GreatestOfFiveNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace GreatestOfFiveNumbers
+{
+    internal class GreatestOfFiveNumbers
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+            int fifthNum = int.Parse(Console.ReadLine());
+
+            int firstGreatest = Math.Max(fifthNum, secondNum);
+            int secondGreatest = Math.Max(firstGreatest, thirdNum);
+            int thirdGreatest = Math.Max(secondGreatest, fourthNum);
+            Console.WriteLine(Math.Max(thirdGreatest, fifthNum));
+        }
+    }
+}
