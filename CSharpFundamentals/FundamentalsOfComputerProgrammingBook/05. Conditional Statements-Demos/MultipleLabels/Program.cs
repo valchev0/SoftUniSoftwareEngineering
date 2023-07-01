3151f1fc7ffbe3c855fba2fe942831b0a649415f
valchev0 <georgi.v@mail.com>
2023-07-01 21:48:03 +0100

    NestedIf

--- CSharpFundamentals/FundamentalsOfComputerProgrammingBook/05. Conditional Statements-Demos/MultipleLabels/Program.cs
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/05. Conditional Statements-Demos/MultipleLabels/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MultipleLabels
+{
+    static void Main()
+    {
+        Console.Write("Please enter some animal: ");
+        string animal = Console.ReadLine();
+
+        switch (animal)
+        {
+            case "dog":
+            case "cat":
+            case "cow":
+                Console.WriteLine("MAMMAL");
+                break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                Console.WriteLine("REPTILE");
+                break;
+            case "hawk":
+            case "sparrow":
+            case "dove":
+                Console.WriteLine("BIRD");
+                break;
+            default:
+                Console.WriteLine("I don't know this animal!");
+                break;
+        }
+    }
+}
