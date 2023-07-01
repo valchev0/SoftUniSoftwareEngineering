7387ddaec2901fff4bff8ba995417fee253f2df7
valchev0 <georgi.v@mail.com>
2023-07-01 21:37:43 +0100

    BonusPoints

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/05. Conditional Statements/BonusPoints/BonusPoints.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace BonusPoints
+{
+    internal class BonusPoints
+    {
+        static void Main(string[] args)
+        {
+            int points = int.Parse(Console.ReadLine());
+            switch (points)
+            {
+                case 1:
+                    case 2:
+                    case 3:
+                    Console.WriteLine(points*10);
+                    break;
+                    case 4:
+                    case 5:
+                    case 6:
+                    Console.WriteLine(points*100);
+                    break;
+                case 7:
+                    case 8:
+                    case 9:
+                    Console.WriteLine(points*1000);
+                    break;
+                    default:
+                    Console.WriteLine("Error!");
+                    break;
+            }
+
+        }
+    }
+}
