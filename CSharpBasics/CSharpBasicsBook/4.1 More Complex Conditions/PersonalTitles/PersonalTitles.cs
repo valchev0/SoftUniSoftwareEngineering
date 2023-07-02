a5b5d628f295f2d7e35387b9a3f9c6cfe1a4960f
valchev0 <georgi.v@mail.com>
2023-07-02 12:05:41 +0100

    PersonalTitles

---
+++ CSharpBasics/CSharpBasicsBook/4.1 More Complex Conditions/PersonalTitles/PersonalTitles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace PersonalTitles
+{
+    internal class PersonalTitles
+    {
+        static void Main(string[] args)
+        {
+
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "f")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else if (age < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+            else if (gender == "m")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else if (age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
