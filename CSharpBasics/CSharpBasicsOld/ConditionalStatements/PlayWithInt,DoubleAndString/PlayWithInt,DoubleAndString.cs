6ae138256317ed930938f6d794ca5b55cc43da77
valchev0 <georgi.v@mail.com>
2023-07-02 10:01:33 +0100

    PlayWithIntDoubleAndString

---
+++ CSharpBasics/CSharpBasicsOld/ConditionalStatements/PlayWithInt,DoubleAndString/PlayWithInt,DoubleAndString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace PlayWithIntDoubleAndString
+{
+    internal class PlayWithIntDoubleAndString
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter variable type (0 - int, 1 - double, 2 string): ");
+            int intVar = Int32.Parse(Console.ReadLine());
+
+            switch (intVar)
+            {
+                case 0:
+                    {
+                        Console.Write("Enter int variable: ");
+                        intVar = Int32.Parse(Console.ReadLine());
+                        intVar++;
+                        Console.WriteLine("Int variable +1 = {0}", intVar);
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.Write("Enter double variable: ");
+                        double doubleVar = double.Parse(Console.ReadLine());
+                        doubleVar++;
+                        Console.WriteLine("Double variable +1 = {0}", doubleVar);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Write("Enter string variable: ");
+                        string stringVar = Console.ReadLine();
+                        stringVar = stringVar + '*';
+                        Console.WriteLine("String variable +* = {0}", stringVar);
+                        break;
+                    }
+                default: Console.WriteLine("Wrong input"); break;
+            }
+        }
+    }
+}
