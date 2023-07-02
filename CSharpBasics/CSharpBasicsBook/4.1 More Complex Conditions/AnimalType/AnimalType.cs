e93edf39f163a7622bf2c4d585bb471450dee596
valchev0 <georgi.v@mail.com>
2023-07-02 12:36:07 +0100

    AnimalType

---
+++ CSharpBasics/CSharpBasicsBook/4.1 More Complex Conditions/AnimalType/AnimalType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AnimalType
+{
+    internal class AnimalType
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+
+            switch (animal)
+
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "reptile":
+                case "tortoise":
+                case "crocodile":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
