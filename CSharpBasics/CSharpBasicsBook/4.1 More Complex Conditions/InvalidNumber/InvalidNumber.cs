751bfbd0c7de278f742d39c12e484c4525ccf43c
valchev0 <georgi.v@mail.com>
2023-07-02 12:19:08 +0100

    InvalidNumber

---
+++ CSharpBasics/CSharpBasicsBook/4.1 More Complex Conditions/InvalidNumber/InvalidNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace InvalidNumber
+{
+    internal class InvalidNumber
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0 || number >= 100 && number <= 200)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
