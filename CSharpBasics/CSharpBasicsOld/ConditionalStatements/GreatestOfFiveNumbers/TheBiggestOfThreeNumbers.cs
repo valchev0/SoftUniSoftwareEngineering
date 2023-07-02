714cfd53cebdfb9d5a7937e3b3ccb54ed3e6422f
valchev0 <georgi.v@mail.com>
2023-07-02 09:41:18 +0100

    TheBiggestOfThreeNumbers

---
+++ CSharpBasics/CSharpBasicsOld/ConditionalStatements/GreatestOfFiveNumbers/TheBiggestOfThreeNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TheBiggestOfThreeNumbers
+{
+    internal class TheBiggestOfThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int firstGreatest = Math.Max(firstNum, secondNum);
+            int secondGreatest = Math.Max(firstGreatest, thirdNum);
+            
+            Console.WriteLine(secondGreatest);
+        }
+    }
+}
