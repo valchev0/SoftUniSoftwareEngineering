b97e84b7e4d210c08845bb10a86c491803279ff5
valchev0 <georgi.v@mail.com>
2023-07-02 09:55:42 +0100

    SortThreeNumbersWithNestedIfs

---
+++ CSharpBasics/CSharpBasicsOld/ConditionalStatements/SortThreeNumbersWithNestedIfs/SortThreeNumbersWithNestedIfs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace SortThreeNumbersWithNestedIfs
+{
+    internal class SortThreeNumbersWithNestedIfs
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            if (firstNum >= secondNum && firstNum >= thirdNum)
+            {
+                Console.Write(firstNum + " ");
+                if (secondNum >= thirdNum)
+                {
+                    Console.Write(secondNum + " ");
+                    Console.Write(thirdNum);
+                }
+                else
+                {
+                    Console.Write(thirdNum + " ");
+                    Console.Write(secondNum);
+                }
+            }
+            else if (secondNum >= firstNum && secondNum >= thirdNum)
+            {
+                Console.Write(secondNum + " ");
+                if (firstNum >= thirdNum)
+                {
+                    Console.Write(firstNum + " ");
+                    Console.Write(thirdNum);
+                }
+                else
+                {
+                    Console.Write(thirdNum + " ");
+                    Console.Write(firstNum);
+                }
+            }
+            else
+            {
+                Console.Write(thirdNum + " ");
+                if (secondNum >= firstNum)
+                {
+                    Console.Write(secondNum + " ");
+                    Console.Write(firstNum);
+                }
+                else
+                {
+                    Console.Write(firstNum + " ");
+                    Console.Write(secondNum);
+                }
+            }
+        }
+    }
+}
