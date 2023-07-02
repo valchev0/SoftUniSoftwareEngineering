84c0d9217e7fa3e349a28207b3eaeb6b06a7be3c
valchev0 <georgi.v@mail.com>
2023-07-02 12:17:27 +0100

    FruitOrVegetable

---
+++ CSharpBasics/CSharpBasicsBook/4.1 More Complex Conditions/FruitOrVegetable/FruitOrVegetable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace FruitOrVegetable
+{
+    internal class FruitOrVegetable
+    {
+        static void Main(string[] args)
+        {
+            string item = Console.ReadLine();
+            switch (item)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    Console.WriteLine("fruit");
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    Console.WriteLine("vegetable");
+
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
