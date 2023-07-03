ef9e89a84b954248bb4ce6b5e10dcb74c80901c2
valchev0 <georgi.v@mail.com>
2023-07-03 19:00:44 +0100

    SleepyTomCat

---
+++ CSharpBasics/CSharpBasicsMoreExercises/ConditionalStatements-More Exercises/PipesInPool/PipesInPool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace PipesInPool
+{
+    internal class PipesInPool
+    {
+        static void Main(string[] args)
+        {
+            int capacity = int.Parse(Console.ReadLine());
+            int flowFirstPipework = int.Parse(Console.ReadLine()); // per hour
+            int flowSecondPipework = int.Parse(Console.ReadLine()); // per hour
+            double hoursForFilling = double.Parse(Console.ReadLine());
+
+            double totalFlowFirst = flowFirstPipework * hoursForFilling;
+            double totalFlowSecond = flowSecondPipework * hoursForFilling;
+            double totalFlow = totalFlowFirst + totalFlowSecond;
+
+            double firstPipeworkPercents = (totalFlowFirst / totalFlow) * 100;
+            double secondPipeworkPercents = (totalFlowSecond / totalFlow) * 100;
+
+            if (totalFlow <= capacity)
+            {
+                double diffrence = (totalFlow / capacity) * 100;
+                Console.WriteLine("The pool is {0:0.00}% full. Pipe 1: {1:0.00}%. Pipe 2: {2:0.00}%.", diffrence, firstPipeworkPercents, secondPipeworkPercents);
+            }
+            else if (totalFlow > capacity)
+            {
+                double overflow = totalFlow - capacity;
+                Console.WriteLine($"For {hoursForFilling:f2} hours the pool overflows with {overflow:f2} liters.");
+            }
+        }
+    }
+}
---
+++ CSharpBasics/CSharpBasicsMoreExercises/ConditionalStatements-More Exercises/SleepyTomCat/SleepyTomCat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace SleepyTomCat
+{
+    internal class SleepyTomCat
+    {
+        static void Main(string[] args)
+        {
+            int normalPlayTime = 30_000;
+            int playPerWorkingDay = 63;
+            int playPerDayOff = 127;
+            int daysPerYear = 365;
+
+            int daysOff = int.Parse(Console.ReadLine());
+            int workingDays = daysPerYear - daysOff;
+            int daysOffMinutes = daysOff * playPerDayOff;
+            int workingDaysMinutes = workingDays * playPerWorkingDay;
+            int playedMinutesTotal = daysOffMinutes + workingDaysMinutes;
+            int diffrence = Math.Abs(playedMinutesTotal - 30_000);
+            int diffrenceHours = diffrence / 60;
+            int diffrenceMinutes = diffrence % 60;
+
+            if (playedMinutesTotal >= 30_000)
+            {
+                Console.WriteLine("Tom will run away \n{0} hours and {1} minutes more for play", diffrenceHours, diffrenceMinutes);
+            }
+            else
+            {
+                Console.WriteLine("Tom sleeps well \n{0} hours and {1} minutes less for play", diffrenceHours, diffrenceMinutes);
+            }
+        }
+    }
+}
