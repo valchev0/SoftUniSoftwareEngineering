14f6fdccf74b19816f510a3cd22310816827dcba
valchev0 <georgi.v@mail.com>
2023-07-04 03:13:09 +0100

    Pets

---
+++ CSharpBasics/CSharpBasicsMoreExercises/ConditionalStatements-More Exercises/Pets/Pets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Pets
+{
+    internal class Pets
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int petsFoodLeft = int.Parse(Console.ReadLine());
+            double dogFoodPerDay = double.Parse(Console.ReadLine());
+            double catFoodPerDay = double.Parse(Console.ReadLine());
+            double turtleFoodPerDay = double.Parse(Console.ReadLine())/1000;
+
+            double dogFoodTotal = dogFoodPerDay * days;
+            double catFoodTotal = catFoodPerDay * days;
+            double turtleFoodTotal = turtleFoodPerDay * days;
+            double petsFoodNeed = dogFoodTotal + catFoodTotal + turtleFoodTotal;
+            double diffrence = Math.Abs(petsFoodNeed - petsFoodLeft);
+
+            if (petsFoodNeed <= petsFoodLeft)
+            {
+                Console.WriteLine("{0} kilos of food left.", Math.Floor(diffrence));
+            }
+            else
+            {
+                Console.WriteLine("{0} more kilos of food are needed.", Math.Ceiling(diffrence));
+            }
+        }
+    }
+}
