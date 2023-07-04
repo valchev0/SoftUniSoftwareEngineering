d3d5d6ddfd563995abdef144d86ae132d627d4b4
valchev0 <georgi.v@mail.com>
2023-07-04 03:43:21 +0100

    Fuel Tank

---
+++ CSharpBasics/CSharpBasicsMoreExercises/ConditionalStatements-More Exercises/FuelTank/FuelTank.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace FuelTank
+{
+    internal class FuelTank
+    {
+        static void Main(string[] args)
+        {
+            string fuelType = Console.ReadLine().ToLower();
+            double amountFuel = double.Parse(Console.ReadLine());
+
+            if (fuelType != "diesel" && fuelType != "gasoline" && fuelType != "gas")
+            {
+                Console.WriteLine("Invalid fuel!");
+            }
+            else if (amountFuel >= 25)
+            {
+                Console.WriteLine("You have enough {0}.", fuelType);
+            }
+            else if(amountFuel < 25)
+            {
+                Console.WriteLine("Fill your tank with {0}!", fuelType);
+            }
+        }
+    }
+}
