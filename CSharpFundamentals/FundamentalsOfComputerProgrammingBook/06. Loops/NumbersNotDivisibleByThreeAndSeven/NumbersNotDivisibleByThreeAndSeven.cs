b098a6ae2059404df9b126d10212b97d928e0d7f
valchev0 <georgi.v@mail.com>
2023-07-05 01:51:07 +0100

    NumbersNotDivisibleByThreeAndSeven

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/NumbersNotDivisibleByThreeAndSeven/NumbersNotDivisibleByThreeAndSeven.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace NumbersNotDivisibleByThreeAndSeven
+{
+    internal class NumbersNotDivisibleByThreeAndSeven
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 || i % 7 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
