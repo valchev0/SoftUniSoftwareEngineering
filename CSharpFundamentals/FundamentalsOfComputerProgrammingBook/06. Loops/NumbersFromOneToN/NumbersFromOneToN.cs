014673871401e492e3fe2ab2c9263f4af52836a6
valchev0 <georgi.v@mail.com>
2023-07-05 01:44:55 +0100

    NumbersFromOneToN

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/NumbersFromOneToN/NumbersFromOneToN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace NumbersFromOneToN
+{
+    internal class NumbersFromOneToN
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
