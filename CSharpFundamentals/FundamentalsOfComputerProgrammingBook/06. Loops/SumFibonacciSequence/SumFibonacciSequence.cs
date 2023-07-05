3c016dae63ba034ce522390461480f68f2549af5
valchev0 <georgi.v@mail.com>
2023-07-05 02:31:56 +0100

    SumFibonacciSequence

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/SumFibonacciSequence/SumFibonacciSequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace SumFibonacciSequence
+{
+    internal class SumFibonacciSequence
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            int lastNum = 0;
+            int sum = 0;
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write(lastNum + " ");
+                num = num + lastNum;
+                lastNum = num - lastNum;
+                sum += lastNum;
+            }
+            Console.WriteLine("\nSum is: {0}", sum);
+        }
+    }
+}
