e71e533bae21bb1b8b5280d1574ede54400d34de
valchev0 <georgi.v@mail.com>
2023-07-05 02:55:46 +0100

    FactorialDivisorAndDivider

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/FactorialNK/FactorialDivisorAndDivider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace FactorialDivisorAndDivider
+{
+    internal class FactorialDivisorAndDivider
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            BigInteger divisor = 1;
+            BigInteger divider = 1;
+            bool isDone = false;
+            BigInteger sum = 0;
+
+            while (!isDone)
+            {
+                while (n > 0)
+                {
+                    divisor *= n;
+                    n--;
+                }
+                Console.WriteLine("Divisor is: {0}", divisor);
+                while (k > 0)
+                {
+                    divider *= k;
+                    k--;
+                }
+                Console.WriteLine("Divider is: {0}", divider);
+                sum = divisor / divider;
+                Console.WriteLine("!N/!K: {0}", sum);
+                isDone = true;
+            }
+        }
+    }
+}
