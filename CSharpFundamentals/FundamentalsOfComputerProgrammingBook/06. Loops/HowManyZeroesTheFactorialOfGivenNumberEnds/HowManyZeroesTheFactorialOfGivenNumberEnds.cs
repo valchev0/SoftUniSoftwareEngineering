c26ea59eaa9c107ad63eb89c8b0d7b3a5e07cf43
valchev0 <georgi.v@mail.com>
2023-07-05 21:20:16 +0100

    HowManyZeroesTheFactorialOfGivenNumberEnds

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/HowManyZeroesTheFactorialOfGivenNumberEnds/HowManyZeroesTheFactorialOfGivenNumberEnds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace HowManyZeroesTheFactorialOfGivenNumberEnds
+{
+    internal class HowManyZeroesTheFactorialOfGivenNumberEnds
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+
+            while(n > 0)
+            {
+                factorial *= n;
+                n--;
+            }
+            Console.WriteLine("Factorial is: {0}", factorial);
+            string str = factorial.ToString();
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            int zerosCounter = 0;
+            foreach(char character in charArray)
+            {
+                if (character ==  '0')
+                {
+                    zerosCounter++;
+                }
+            }
+            Console.WriteLine(zerosCounter);
+        }
+    }
+}
