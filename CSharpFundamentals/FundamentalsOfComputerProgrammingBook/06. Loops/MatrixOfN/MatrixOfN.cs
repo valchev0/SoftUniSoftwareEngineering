0ca54d13af4cb22526cd5b32eaa73505b63c109c
valchev0 <georgi.v@mail.com>
2023-07-05 15:28:04 +0100

    MatrixOfN

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/MatrixOfN/MatrixOfN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MatrixOfN
+{
+    internal class MatrixOfN
+    {
+        static void Main(string[] args)
+        {
+            //not completed
+            int n = int.Parse(Console.ReadLine());
+            int stopper = 0;
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int columns = rows; columns <= n + stopper; columns++)
+                {
+                    Console.Write(columns + " ");
+                }
+                Console.WriteLine();
+                stopper++;
+            }
+        }
+    }
+}
