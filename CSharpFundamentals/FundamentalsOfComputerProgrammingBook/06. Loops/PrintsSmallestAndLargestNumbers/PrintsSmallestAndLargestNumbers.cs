0deb3c8d5ec181e988b3e54f2ce8cf8ba30acfcf
valchev0 <georgi.v@mail.com>
2023-07-05 01:56:36 +0100

    PrintsSmallestAndLargestNumbers

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/PrintsSmallestAndLargestNumbers/PrintsSmallestAndLargestNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace PrintsSmallestAndLargestNumbers
+{
+    internal class PrintsSmallestAndLargestNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue; 
+            int min = int.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (currentNum > max)
+                {
+                    max = currentNum;
+                }
+                if (currentNum < min)
+                {
+                    min = currentNum;
+                }
+            }
+            Console.WriteLine("Max number is {0}\nMin number is {1}", max, min);
+        }
+    }
+}
