f2f67ead14b650e6e38c8646fbe1beafbbee7202
valchev0 <georgi.v@mail.com>
2023-07-05 23:31:24 +0100

    BinaryToDecimal

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BinaryToDecimal
+{
+    internal class BinaryToDecimal
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string str = n.ToString();
+            int decimalNum = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '1')
+                {
+                    decimalNum += 1 * ((int)Math.Pow(2, (str.Length - (i + 1))));
+                }
+            }
+            Console.WriteLine(decimalNum);
+        }
+    }
+}
