2ea16de45f6ccb1c7b509fca327b7c7711d7bad0
valchev0 <georgi.v@mail.com>
2023-07-06 23:32:10 +0100

    RandomOrder

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/RandomOrder/RandomOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace RandomOrder
+{
+    internal class RandomOrder
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            while (n > 0) 
+            {
+                Console.WriteLine(rnd.Next(n+1));
+                n--;
+            }
+        }
+    }
+}
