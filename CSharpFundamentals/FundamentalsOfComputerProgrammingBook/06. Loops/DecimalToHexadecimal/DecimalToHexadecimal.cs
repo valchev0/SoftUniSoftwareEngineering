2a6e5e44340e3a8a5d42d0b2a5ede8f21c3aa2a0
valchev0 <georgi.v@mail.com>
2023-07-06 18:15:50 +0100

    HexadecimalToBinary

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/DecimalToHexadecimal/DecimalToHexadecimal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Numerics;
+
+namespace DecimalToHexadecimal
+{
+    internal class DecimalToHexadecimal
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int numCopy = n;
+            int diffrence = 0;
+            string reversedNumber = string.Empty;
+            string hexadecimal = string.Empty;
+            while (numCopy > 0)
+            {
+                diffrence = numCopy % 16;
+                numCopy /= 16;
+                if (diffrence >= 0 && diffrence < 10)
+                {
+                    reversedNumber += diffrence;
+                }
+                else if (diffrence == 10)
+                {
+                    reversedNumber += "a";
+                }
+                else if(diffrence == 11)
+                {
+                    reversedNumber += "b";
+                }
+                else if(diffrence == 12)
+                {
+                    reversedNumber += "c";
+                }
+                else if(diffrence == 13)
+                {
+                    reversedNumber += "d";
+                }
+                else if(diffrence == 14)
+                {
+                    reversedNumber += "e";
+                }
+                else if(diffrence == 15)
+                {
+                    reversedNumber += "f";
+                }
+            }
+            for (int i = reversedNumber.Length - 1; i >= 0; i--)
+            {
+                hexadecimal += reversedNumber[i];
+            }
+            Console.WriteLine(hexadecimal.ToUpper());
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops/HexadecimalToBinary/HexadecimalToBinary.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Numerics;
+
+namespace HexadecimalToBinary
+{
+    internal class HexadecimalToBinary
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string binary = string.Empty;
+            string bin = string.Empty;
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string hex = new string(charArray);
+            char? currentChar = null;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                currentChar = input[i];
+                switch (currentChar)
+                {
+                    case '0':
+                        bin += "0000";
+                        break;
+                    case '1':
+                        bin += "0001";
+                        break;
+                    case '2':
+                        bin += "0010";
+                        break;
+                    case '3':
+                        bin += "0011";
+                        break;
+                    case '4':
+                        bin += "0100";
+                        break;
+                    case '5':
+                        bin += "0101";
+                        break;
+                    case '6':
+                        bin += "0110";
+                        break;
+                    case '7':
+                        bin += "0111";
+                        break;
+                    case '8':
+                        bin += "1000";
+                        break;
+                    case '9':
+                        bin += "1001";
+                        break;
+                    case 'a':
+                        bin += "1010";
+                        break;
+                    case 'b':
+                        bin += "1011";
+                        break;
+                    case 'c':
+                        bin += "1100";
+                        break;
+                    case 'd':
+                        bin += "1101";
+                        break;
+                    case 'e':
+                        bin += "1110";
+                        break;
+                    case 'f':
+                        bin += "1111";
+                        break;
+                }
+            }
+            binary = bin;
+            Console.WriteLine(binary);
+        }
+    }
+}
