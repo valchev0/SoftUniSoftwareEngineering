685c2792abc077deae90a4bbb5a4a4d92c61e974
valchev0 <georgi.v@mail.com>
2023-07-07 16:50:19 +0100

    SumOdd

---
+++ CSharpBasics/CSharpBasicsOld/Loops/00. Demos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 15;
+            for (int row = 0; row <= n; row++)
+            {
+                int spacesCount = (n - row);
+                string spaces = new string(' ', spacesCount);
+                Console.Write(spaces);
+                for (int col = 2; col <= row; col++)
+                {
+                    Console.Write("*");
+                    if (col < row)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
