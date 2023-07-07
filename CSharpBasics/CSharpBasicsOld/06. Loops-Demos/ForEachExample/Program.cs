692e31f1b3b61db5c54c1ebecbb7ed559d291fc0
valchev0 <georgi.v@mail.com>
2023-07-07 16:56:54 +0100

    ForLoops

--- CSharpBasics/CSharpBasicsOld/06. Loops-Demos/ForEachExample/Program.cs
+++ CSharpBasics/CSharpBasicsOld/06. Loops-Demos/ForEachExample/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class ForEachExample
+{
+    static void Main()
+    {
+        string[] days = new string[] {
+            "Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday", "Sunday"
+        };
+        foreach (String day in days)
+        {
+            Console.WriteLine(day);
+        }
+    }
+}
