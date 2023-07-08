a9f8f9c8ee60f7464aa3d4f6b177f35448a06e2a
valchev0 <georgi.v@mail.com>
2023-07-08 16:03:45 +0100

    LatinLetters

---
+++ CSharpBasics/CSharpBasicsBook/5.1. Loops/LatinLetters/LatinLetters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LatinLetters
+{
+    internal class LatinLetters
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Latin alphabet:");
+            for (char letter = 'a'; letter <= 'z'; letter++)
+            {
+                    Console.WriteLine(" " + letter);
+            }
+            Console.WriteLine();
+        }
+    }
+}
