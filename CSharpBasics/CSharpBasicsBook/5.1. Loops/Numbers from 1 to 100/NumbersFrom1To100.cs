35bbc90e4cac8455a64aca4a5cf8672c11861889
valchev0 <georgi.v@mail.com>
2023-07-08 15:52:41 +0100

    NumbersFrom1To100

---
+++ CSharpBasics/CSharpBasicsBook/5.1. Loops/Numbers from 1 to 100/NumbersFrom1To100.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NumbersFrom1To100
+{
+    internal class NumbersFrom1To100
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
