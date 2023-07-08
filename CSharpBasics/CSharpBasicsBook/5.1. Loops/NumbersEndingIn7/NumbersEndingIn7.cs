ad4fa95722a363dc6940bf7e24f77273a7e10e79
valchev0 <georgi.v@mail.com>
2023-07-08 15:55:25 +0100

    NumbersEndingIn7

---
+++ CSharpBasics/CSharpBasicsBook/5.1. Loops/NumbersEndingIn7/NumbersEndingIn7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace NumbersEndingIn7
+{
+    internal class NumbersEndingIn7
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
