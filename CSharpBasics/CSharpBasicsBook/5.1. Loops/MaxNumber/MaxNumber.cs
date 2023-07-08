1ac21935136f5bf9ec6d3956843e7408a70d8694
valchev0 <georgi.v@mail.com>
2023-07-08 17:48:09 +0100

    MaxNumber

---
+++ CSharpBasics/CSharpBasicsBook/5.1. Loops/MaxNumber/MaxNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace MaxNumber
+{
+    internal class MaxNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("n = ");
+            int n = int.Parse(Console.ReadLine());
+            var max = -100000000000;
+            for (int i = 0; i < n; i++)
+            {
+                var currentNum = int.Parse(Console.ReadLine());
+                if(max < currentNum)
+                {
+                    max = currentNum;
+                }
+            }
+            Console.WriteLine("max = {0}", max);
+        }
+    }
+}
