0d13acb2aaed6e056016c77f72be3e52fa8956cc
valchev0 <georgi.v@mail.com>
2023-07-09 06:37:31 +0100

    HalfSumElement

---
+++ CSharpBasics/CSharpBasicsBook/5.1. Loops/HalfSumElement/HalfSumElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace HalfSumElement
+{
+    internal class HalfSumElement
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = 0;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (currentNum > max)
+                {
+                    max = currentNum;
+                }
+                sum += currentNum;
+            }
+            int clearSum = Math.Abs(sum - max);
+            if (clearSum == max)
+            {
+                Console.WriteLine("Yes \nSum = {0}", clearSum);
+            }
+            else
+            {
+                int diffrence = Math.Abs(clearSum - max);
+                Console.WriteLine("No \nDiff = {0}", diffrence);
+            }
+        }
+    }
+}
