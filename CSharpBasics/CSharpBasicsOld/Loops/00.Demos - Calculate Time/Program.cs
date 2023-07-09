10740b05e4d8cac7724c7a948308470fb6ae8b6c
valchev0 <georgi.v@mail.com>
2023-07-09 08:49:20 +0100

    00.Demos - Calculate Time

---
+++ CSharpBasics/CSharpBasicsOld/Loops/00.Demos - Calculate Time/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTime = DateTime.Now;
+            for (int i = 0; i < 100000; i++)
+            {
+
+            }
+            var endTime = DateTime.Now;
+            Console.WriteLine(endTime - startTime);
+        }
+    }
+}
