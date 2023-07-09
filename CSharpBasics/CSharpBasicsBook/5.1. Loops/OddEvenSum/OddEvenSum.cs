83cd06fb255bc7d7c92f459be37856538bfef554
valchev0 <georgi.v@mail.com>
2023-07-09 06:13:34 +0100

    OddEvenSum

---
+++ CSharpBasics/CSharpBasicsBook/5.1. Loops/OddEvenSum/OddEvenSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace OddEvenSum
+{
+    internal class OddEvenSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += currentNum;
+                }
+                else
+                {
+                    oddSum += currentNum;
+                }
+            }
+            int diffrence = Math.Abs(oddSum - evenSum);
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine("Yes \nSum = {0}", oddSum);
+            }
+            else
+            {
+                Console.WriteLine("No \nDiff = {0}", diffrence);
+            }
+        }
+    }
+}
