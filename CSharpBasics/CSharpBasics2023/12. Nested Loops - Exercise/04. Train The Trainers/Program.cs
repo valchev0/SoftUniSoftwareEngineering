069f3b6407d2068a8475d8e9573b9483bbc9920d
valchev0 <georgi.v@mail.com>
2023-07-11 18:46:52 +0100

    12. Nested Loops - Exercise

---
+++ CSharpBasics/CSharpBasics2023/12. Nested Loops - Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _04.TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInJury = int.Parse(Console.ReadLine());
+            string nameOfPresentation = Console.ReadLine();
+
+            double averageScoreForAllPresentation = 0;
+            int counter = 0;
+
+            while (nameOfPresentation != "Finish")
+            {
+                double averageScore = 0;
+
+                for (int i = 0; i < peopleInJury; i++)
+                {
+                    double currentScore = double.Parse(Console.ReadLine());
+
+                    averageScore += currentScore;
+                    averageScoreForAllPresentation += currentScore;
+                    counter++;
+                }
+
+                double averageScorePerPresentation = averageScore / peopleInJury;
+
+                Console.WriteLine($"{nameOfPresentation} - {averageScorePerPresentation:F2}.");
+
+                nameOfPresentation = Console.ReadLine();
+            }
+
+            double assesment = averageScoreForAllPresentation / counter;
+
+            Console.WriteLine($"Student's final assessment is {assesment:F2}.");
+
+
+            //OR
+            //int jury = int.Parse(Console.ReadLine());
+            //string task;
+            //int tasksCounter = 0;
+            //double totalGrades = 0;
+            //int gradeCounter = 0;
+            //while ((task = Console.ReadLine()) != "Finish")
+            //{
+            //    double taskGrade = 0;
+            //    tasksCounter = 0;
+            //    while (tasksCounter < jury)
+            //    {
+            //        double currentGrade = double.Parse(Console.ReadLine());
+            //        taskGrade += currentGrade;
+            //        tasksCounter++;
+            //    }
+            //    gradeCounter += tasksCounter;
+            //    totalGrades += taskGrade;
+            //    Console.WriteLine($"{task} - {(taskGrade / jury):f2}.");
+            //}
+            //Console.WriteLine($"Student's final assessment is {(totalGrades / gradeCounter):f2}.");
+        }
+    }
+}
---
+++ CSharpBasics/CSharpBasics2023/12. Nested Loops - Exercise/06. Cinema Tickets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace P06.CinemaTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            int primeSum = 0;
+            int nonPrimeSum = 0;
+            while ((command = Console.ReadLine()) != "stop")
+            {
+                int number = int.Parse(command);
+                if (number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    continue;
+                }
+                if ((number == 1) || (number == 0))
+                {
+                    nonPrimeSum += number;
+                    continue;
+                }
+                bool isPrime = true;
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+                if (isPrime)
+                {
+                    primeSum+=number;
+                }
+                else
+                {
+                    nonPrimeSum+=number;
+                }
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+        }
+    }
+}
