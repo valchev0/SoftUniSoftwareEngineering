7d305e79ee0e628a9ba86e0624f70537673b67ea
valchev0 <georgi.v@mail.com>
2023-07-22 05:27:39 +0100

    Rectangle

---
+++ CSharpBasics/CSharpBasicsBook/6.1. Nested Loops/Rectangle of N x N Stars/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Rectangle
+{
+    internal class Rectangle
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharpBasics/CSharpBasicsBook/6.1. Nested Loops/Rectangle/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Rectangle
+{
+    internal class Rectangle
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
