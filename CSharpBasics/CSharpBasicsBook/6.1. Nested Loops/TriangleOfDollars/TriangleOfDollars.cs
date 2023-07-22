7de43339f9a6d5d1a245a69af5b778a4547b2584
valchev0 <georgi.v@mail.com>
2023-07-22 05:37:31 +0100

    Triangle of Dollars

---
+++ CSharpBasics/CSharpBasicsBook/6.1. Nested Loops/TriangleOfDollars/TriangleOfDollars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TriangleOfDollars
+{
+    internal class TriangleOfDollars
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int rows = 0; rows < n; rows++)
+            {
+                Console.Write("$");
+                for (int cols = 0; cols < rows; cols++)
+                {
+                    Console.Write(" $");
+                    //if (cols != (n - 1))
+                    //{
+                    //    Console.Write(" ");
+                    //}
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
