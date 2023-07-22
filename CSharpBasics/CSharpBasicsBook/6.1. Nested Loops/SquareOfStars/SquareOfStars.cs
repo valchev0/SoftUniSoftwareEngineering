7100fcc3f2cc8140d43eef5791577ca0ff009625
valchev0 <georgi.v@mail.com>
2023-07-22 05:32:30 +0100

    SquareOfStars

---
+++ CSharpBasics/CSharpBasicsBook/6.1. Nested Loops/SquareOfStars/SquareOfStars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SquareOfStars
+{
+    internal class SquareOfStars
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("*");
+                    if (j != (n-1))
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
