32e848ee6ce38bc18281f25c874ed024d6a11d60
valchev0 <georgi.v@mail.com>
2023-07-29 15:04:37 +0100

    Rhombus of Stars

--- CSharpBasics/CSharpBasicsBook/6.1. Nested Loops/RhombusOfStars/RhombusOfStars.cs
+++ CSharpBasics/CSharpBasicsBook/6.1. Nested Loops/RhombusOfStars/RhombusOfStars.cs
@@ -7,20 +7,30 @@ namespace RhombusOfStars
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            for (int row = 1; row <= n; row++)
+            Top(n);
+            Bottom(n);
+        }
+
+        private static void Bottom(int n)
+        {
+            for (int row = n - 1; row >= 1; row--)
             {
                 for (int col = 1; col <= n - row; col++)
                 {
                     Console.Write(" ");
                 }
                 Console.Write("*");
-                for (int col = 1;col < row; col++)
+                for (int col = 1; col < row; col++)
                 {
                     Console.Write(" *");
                 }
                 Console.WriteLine();
             }
-            for (int row = n - 1; row >= 1; row--)
+        }
+
+        private static void Top(int n)
+        {
+            for (int row = 1; row <= n; row++)
             {
                 for (int col = 1; col <= n - row; col++)
                 {
