33682f1c120ee2fc1dae10d60fd9ef1a20c9974e
valchev0 <georgi.v@mail.com>
2023-07-30 08:04:18 +0100

    Christmas Tree

---
+++ CSharpBasics/CSharpBasicsBook/6.1. Nested Loops/ChristmasTree/ChristmasTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChristmasTree
+{
+    internal class ChristmasTree
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int row = 0; row <= n; row++)
+            {
+                for (int col = 0; col < n - row; col++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < row; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write(" | ");
+                for (int i = 0; i < row; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
