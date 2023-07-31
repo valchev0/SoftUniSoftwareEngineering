e548ee15ab7b4cefe6a1172d3c8bcb4ba7e246a1
valchev0 <georgi.v@mail.com>
2023-07-31 20:39:21 +0100

    Arrow

---
+++ CSharpBasics/CSharpBasicsBook/6.2. Drawing on the Console - Exam Problems/Arrow/Arrow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Arrow
+{
+    internal class Arrow
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}{1}{0}",
+                new string('.', ((n * 2 - 1) - n) / 2),
+                new string('#', n));//1ST PART - STATIC
+
+
+            for (int i = 0; i < n - 2; i++)//2ND PART
+            {
+                Console.WriteLine("{0}{1}{2}{1}{0}",
+                    new string('.', n / 2),
+                    new string('#', 1),
+                    new string('.', n - 2));
+            }
+
+            Console.WriteLine("{0}{1}{0}",
+                new string('#', n / 2 + 1),
+                new string('.', ((n * 2 - 1) - 2 * (n / 2 + 1))));//3RD PART - STATIC
+
+            for (int i = 0; i < n - 2; i++)//4TH PART
+            {
+                Console.WriteLine("{0}{1}{2}{1}{0}",
+                    new string('.', i + 1),
+                    new string('#', 1),
+                    new string('.', ((n * 2 - 1) - 4 - (i * 2))));
+            }
+            Console.WriteLine("{0}{1}{0}",
+                new string('.', n - 1),
+                new string('#', 1));//5TH PART - STATIC
+        }
+    }
+}
