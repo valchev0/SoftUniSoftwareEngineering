ddc9bc4aa730bb0fca28522a2cfba11cab43b35e
valchev0 <georgi.v@mail.com>
2023-07-31 23:48:00 +0100

    Numbers N...1

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/NumbersNToOne/NumbersNToOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumbersNToOne
+{
+    internal class NumbersNToOne
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
