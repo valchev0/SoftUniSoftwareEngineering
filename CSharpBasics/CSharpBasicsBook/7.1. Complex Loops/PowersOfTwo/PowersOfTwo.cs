f960edb68052986cdd730a9ea463174c0291dfd4
valchev0 <georgi.v@mail.com>
2023-07-31 23:52:45 +0100

    Powers of Two

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/PowersOfTwo/PowersOfTwo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NumbersOneNwithStepThree
+{
+    internal class NumbersOneNwithStepThree
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            for (int i = 0; i <= n; i ++)
+            {
+                Console.WriteLine(i);
+                num *= 2;
+            }
+        }
+    }
+}
