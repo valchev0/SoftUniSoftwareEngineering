0823b33556b3f31286273dbf68c395e9a1032a81
valchev0 <georgi.v@mail.com>
2023-08-01 00:17:32 +0100

    Sequence 2k+1

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/Sequence2k+1/Sequence2k+1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sequence2kPlus1
+{
+    internal class Sequence2kPlus1
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            while (num <= n)
+            {
+                Console.WriteLine(num);
+                num = (2 * num) + 1;
+            }
+        }
+    }
+}
