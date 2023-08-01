b83cfec05d8f3983323aa94120b1f01b5e60207f
valchev0 <georgi.v@mail.com>
2023-08-01 20:09:10 +0100

    Sum Digits

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/SumDigits/SumDigits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumDigits
+{
+    internal class SumDigits
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = n;
+            int sum = 0;
+            while (num > 0)
+            {
+                int lastDigit = num % 10;
+                sum += lastDigit;
+                num /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
