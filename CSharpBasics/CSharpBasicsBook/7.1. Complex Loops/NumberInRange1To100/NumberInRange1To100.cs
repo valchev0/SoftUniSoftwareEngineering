b701be8a731d91a2eb3d56fd538aa31865c77d32
valchev0 <georgi.v@mail.com>
2023-08-01 15:16:32 +0100

    Number in Range [1...100]

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/NumberInRange1To100/NumberInRange1To100.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumberInRange1To100
+{
+    internal class NumberInRange1To100
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            bool inRange = false;
+            while (inRange == false)
+            {
+                if (num >= 1 && num <= 100)
+                {
+                    inRange = true; 
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number!");
+                }
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
