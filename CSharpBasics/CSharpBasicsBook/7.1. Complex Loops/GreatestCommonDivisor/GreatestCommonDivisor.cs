ba25784b5251af136b64b32f8f053da7b68eaeb0
valchev0 <georgi.v@mail.com>
2023-08-01 15:45:28 +0100

    Greatest Common Divisor (CGD)

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/GreatestCommonDivisor/GreatestCommonDivisor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GreatestCommonDivisor
+{
+    internal class GreatestCommonDivisor
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int min = Math.Min(a, b);
+            int gcd = 0;
+
+            for (int i = 1; i <= min; i++)
+            {
+                if ((a % i == 0) && (b % i == 0))
+                {
+                    gcd = i;
+                }
+            }
+            Console.WriteLine("GCD = {0}", gcd);
+        }
+    }
+}
