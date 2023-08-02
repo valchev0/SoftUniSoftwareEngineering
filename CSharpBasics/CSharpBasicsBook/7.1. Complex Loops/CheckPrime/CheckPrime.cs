315b06bf29da59fba045a9c5e7cd95219d4aac8f
valchev0 <georgi.v@mail.com>
2023-08-02 15:40:25 +0100

    Enter Even Number

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/CheckPrime/CheckPrime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CheckPrime
+{
+    internal class CheckPrime
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if(n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
+        }
+    }
+}
---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/EnterEvenNumber/EnterEvenNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EnterEvenNumber
+{
+    internal class EnterEvenNumber
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            bool isEven = false;
+            while (!isEven)
+            {
+                n = int.Parse(Console.ReadLine());
+                if (n % 2 == 0)
+                {
+                    isEven = true;
+                    break;
+                }
+                Console.WriteLine("The number is not even.");
+            }
+            Console.WriteLine("Even number entered: {0}", n);
+
+        }
+    }
+}
