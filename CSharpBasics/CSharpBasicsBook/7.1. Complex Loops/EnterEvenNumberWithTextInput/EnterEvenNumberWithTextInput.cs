e6a72167b47bff8de03b9c904b2b2950edb1bdc9
valchev0 <georgi.v@mail.com>
2023-08-03 17:11:18 +0100

    Enter Even Number (with Text Input)

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/EnterEvenNumberWithTextInput/EnterEvenNumberWithTextInput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EnterEvenNumberWithTextInput
+{ 
+
+    internal class EnterEvenNumberWithTextInput
+    {
+        static void Main(string[] args)
+        {
+			while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter even number: ");
+                    int n = int.Parse(Console.ReadLine());
+                    if (n % 2 == 0)
+                    {
+                        Console.WriteLine("Even number entered: {0}", n);
+                        break;
+                    }
+                    Console.WriteLine("The number is not even.");
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid number.");
+                }
+            }
+        }
+    }
+}
