a13d929ae3a315e4e9ac24e87f940eb1294104f6
valchev0 <georgi.v@mail.com>
2023-08-06 21:25:08 +0100

    Fibonacci

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/Fibonacci/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int factZero = 1;
+            int factOne = 1;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int factNext = factZero + factOne;
+                factZero = factOne;
+                factOne = factNext;
+            }
+            Console.WriteLine(factOne);
+        }
+    }
+}
