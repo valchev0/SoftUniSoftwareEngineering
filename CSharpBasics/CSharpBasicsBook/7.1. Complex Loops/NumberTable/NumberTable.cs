e52ec4ec2faff18d782e40e5e31620e550e6e1d3
valchev0 <georgi.v@mail.com>
2023-08-13 09:45:14 +0100

    Number Table

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/NumberTable/NumberTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumberTable
+{
+    internal class NumberTable
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            for (int row = 0; row < n; row ++)
+            {
+                for (int col = 0; col <= n; col++)
+                {
+                    num = row + col;
+                    if (num >= n)
+                    {
+                        num = -num + 2 * n;
+
+                    }
+                    if (col > 0)
+                    {   
+                        Console.Write(num + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
