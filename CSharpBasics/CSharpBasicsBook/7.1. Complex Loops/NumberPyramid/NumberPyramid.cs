7b9f50f32e08bdf74ba6109be336ce51650dcda5
valchev0 <georgi.v@mail.com>
2023-08-13 09:20:09 +0100

    Number Pyramid

---
+++ CSharpBasics/CSharpBasicsBook/7.1. Complex Loops/NumberPyramid/NumberPyramid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumberPyramid
+{
+    internal class NumberPyramid
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    if (col > 1)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(num);
+                    num++;
+                    if (num > n)
+                    {
+                        break;
+                    }
+                }
+                if (num > n)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
