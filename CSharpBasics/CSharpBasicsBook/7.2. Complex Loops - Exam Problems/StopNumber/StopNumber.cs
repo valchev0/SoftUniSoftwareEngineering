f8651ae47989162446400c9bf5c75d0559ce7d13
valchev0 <georgi.v@mail.com>
2023-08-14 17:26:48 +0100

    Stop Number

---
+++ CSharpBasics/CSharpBasicsBook/7.2. Complex Loops - Exam Problems/StopNumber/StopNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StopNumber
+{
+    internal class StopNumber
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int stopNumber = int.Parse(Console.ReadLine());
+
+            for (int i = secondNum; i >= firstNum; i--)
+            {
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    if (i == stopNumber)
+                    {
+                        break;
+                    }
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
