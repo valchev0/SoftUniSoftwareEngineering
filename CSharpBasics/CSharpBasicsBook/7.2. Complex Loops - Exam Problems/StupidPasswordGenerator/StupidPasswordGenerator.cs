15e2cd24efea3a4ad58429f9ef8141e05be28a08
valchev0 <georgi.v@mail.com>
2023-08-14 16:34:40 +0100

    Stupid Password Generator

---
+++ CSharpBasics/CSharpBasicsBook/7.2. Complex Loops - Exam Problems/StupidPasswordGenerator/StupidPasswordGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StupidPasswordGenerator
+{
+    internal class StupidPasswordGenerator
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < firstNum; i++)
+            {
+                for (int j = 1; j < firstNum; j++)
+                {
+                    for(int k = 97; k < 97 + secondNum; k++)
+                    {
+                        for (int l = 97; l < 97 + secondNum; l++)
+                        {
+                            for (int m = Math.Max(i, j) + 1; m <= firstNum; m++)
+                            {
+                                Console.Write($"{i}{j}{(char)k}{(char)l}{m} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
