e64f2b0f6b1e7f400d5a2b93ddf1fc162406da84
valchev0 <georgi.v@mail.com>
2023-08-21 18:53:37 +0100

    Point on Segment

---
+++ CSharpBasics/CSharpBasicsBook/8.1. Practical Exam Preparation/PointOnSegment/PointOnSegment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PointOnSegment
+{
+    internal class PointOnSegment
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine()); 
+            int point = int.Parse(Console.ReadLine());  
+
+            if (point >= Math.Min(first, second) && point <= Math.Max(first, second))
+            {
+                Console.WriteLine("in");
+            }
+            else
+            {
+                Console.WriteLine("out");
+            }
+            int firstDiffrence = Math.Abs(first - point);
+            int secondDiffrence = Math.Abs(second - point);
+
+            if (firstDiffrence < secondDiffrence)
+            {
+                Console.WriteLine(firstDiffrence);
+            }
+            else
+            {
+                Console.WriteLine(secondDiffrence);
+            }
+        }
+    }
+}
