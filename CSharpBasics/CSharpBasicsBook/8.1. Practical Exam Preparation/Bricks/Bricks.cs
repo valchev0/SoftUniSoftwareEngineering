b6ee4fe230445762be96a036247bdc2bb337d96f
valchev0 <georgi.v@mail.com>
2023-08-21 01:56:17 +0100

    Bricks

---
+++ CSharpBasics/CSharpBasicsBook/8.1. Practical Exam Preparation/Bricks/Bricks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bricks
+{
+    internal class Bricks
+    {
+        static void Main(string[] args)
+        {
+            double bricks = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+            int wheelborrowCapacity = int.Parse(Console.ReadLine());
+
+            double circles = (double)Math.Ceiling(bricks / (wheelborrowCapacity * workers));
+            Console.WriteLine(circles);
+        }
+    }
+}
