65ed4dba290c39f7e4f0fa32013d676b01c28be9
valchev0 <georgi.v@mail.com>
2023-08-22 17:53:29 +0100

    Sums 3 Numbers

---
+++ CSharpBasics/CSharpBasicsBook/8.1. Practical Exam Preparation/SumsThreeNumbers/SumsThreeNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumsThreeNumbers
+{
+    internal class SumsThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int min = Math.Min(thirdNum, Math.Min(firstNum, secondNum));
+            int max = Math.Max(thirdNum, Math.Max(firstNum, secondNum));
+            int diffrence = max - min;
+
+            if (diffrence == firstNum || diffrence == secondNum || diffrence == thirdNum)
+            {
+                Console.WriteLine("{0} + {1} = {2}", min, diffrence, max);
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
