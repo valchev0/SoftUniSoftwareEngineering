826386a8c27c3041a782b21560e8347d63a93519
valchev0 <georgi.v@mail.com>
2023-08-22 18:35:09 +0100

    Increasing Elements

---
+++ CSharpBasics/CSharpBasicsBook/8.1. Practical Exam Preparation/IncreasingElements/IncreasingElements.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IncreasingElements
+{
+    internal class IncreasingElements
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int increasingElements = 0;
+            int prev = 0;
+            int currentNum = 0;
+            int maxSequence = 0;
+            for (int i = 0; i < n; i++)
+            {
+                currentNum = int.Parse(Console.ReadLine());
+                if (currentNum > prev)
+                {
+                    increasingElements++; 
+                }
+                else
+                {
+                    increasingElements = 1;
+                }
+                prev = currentNum;
+                if (maxSequence < increasingElements)
+                {
+                    maxSequence = increasingElements;
+                }
+            }
+            Console.WriteLine(maxSequence);
+        }
+    }
+}
