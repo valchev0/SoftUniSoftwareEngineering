badd4fa99c0bb0c9e3b2533b777c9e7732886816
valchev0 <georgi.v@mail.com>
2023-08-22 18:01:23 +0100

    Sums Step 3

---
+++ CSharpBasics/CSharpBasicsBook/8.1. Practical Exam Preparation/SumsStepThree/SumsStepThree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumsStepThree
+{
+    internal class SumsStepThree
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int firstSum = 0;
+            int secondSum = 0;
+            int thirdSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (i % 3 == 1)
+                {
+                    firstSum += currentNum;
+                }
+                else if (i % 3 == 2)
+                { 
+                    secondSum += currentNum; 
+                }
+                else
+                {
+                    thirdSum += currentNum;
+                }
+            }
+            Console.WriteLine("sum1 = {0} \nsum2 = {1} \nsum3 = {2}", firstSum, secondSum, thirdSum);
+        }
+    }
+}
