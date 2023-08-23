04c2051e76a8dc696054fcf36b6499c6bf5aee83
valchev0 <georgi.v@mail.com>
2023-08-23 19:34:53 +0100

    Perfect Diamond

---
+++ CSharpBasics/CSharpBasicsBook/8.1. Practical Exam Preparation/PerfectDiamond/PerfectDiamond.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PerfectDiamond
+{
+    internal class PerfectDiamond
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int rows = 1; rows <= n; rows++)
+            {
+
+                Console.Write(new string(' ', ((n - rows))));
+                Console.Write("*");
+                if (rows > 0)
+                {
+                    for (int cols = 1; cols < rows; cols++)
+                    {
+                        Console.Write("-*");
+                    }
+                }
+                Console.WriteLine();
+            }
+            for (int rows = n - 1; rows > 0; rows--)
+            {
+                Console.Write(new string(' ', ((n - rows))));
+
+                Console.Write("*");
+                if (rows > 0)
+                {
+                    for (int cols = 1; cols < rows; cols++)
+                    {
+                        Console.Write("-*");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
