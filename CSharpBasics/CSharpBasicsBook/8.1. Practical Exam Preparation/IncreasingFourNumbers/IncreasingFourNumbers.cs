8947f59188b3668bc7e3435abbb8f59589fdb830
valchev0 <georgi.v@mail.com>
2023-08-24 18:04:54 +0100

    Increasing 4 Numbers

---
+++ CSharpBasics/CSharpBasicsBook/8.1. Practical Exam Preparation/IncreasingFourNumbers/IncreasingFourNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+
+namespace IncreasingFourNumbers
+{
+    internal class IncreasingFourNumbers
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int count = 0;
+            
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = i + 1; j <= end; j++)
+                {
+                    for(int k = j + 1; k <= end; k++)
+                    {
+                        for (int l = k + 1; l <= end; l++)
+                        {
+                            Console.WriteLine("{0} {1} {2} {3}", i, j, k, l);
+                            count++;
+                        }
+                    }
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
