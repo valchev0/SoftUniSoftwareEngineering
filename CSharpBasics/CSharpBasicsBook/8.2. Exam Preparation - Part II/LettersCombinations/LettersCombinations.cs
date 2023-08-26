877d0194a4d374a264259000e0c69d21be492bac
valchev0 <georgi.v@mail.com>
2023-08-26 11:37:25 +0100

    Letters Combinations

---
+++ CSharpBasics/CSharpBasicsBook/8.2. Exam Preparation - Part II/LettersCombinations/LettersCombinations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LettersCombinations
+{
+    internal class LettersCombinations
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            char pass = char.Parse(Console.ReadLine());
+            char lastChar = first;
+            int counter = 0;
+
+            for (int j = first; j <= end; j++)
+            {
+                if (j == pass)
+                {
+                    continue;
+                }
+                for (int k = first; k <= end; k++)
+                {
+                    if (k == pass)
+                    {
+                        continue;
+                    }
+                    for (int l = lastChar; l <= end; l++)
+                    {
+                        if (l == pass)
+                        {
+                            continue;
+                        }
+                        Console.Write("{0}{1}{2} ", (char)j, (char)k, (char)l);
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
