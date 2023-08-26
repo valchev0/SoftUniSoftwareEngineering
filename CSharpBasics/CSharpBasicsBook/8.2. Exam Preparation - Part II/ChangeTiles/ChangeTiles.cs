0a87432fa2ffd5082f00382bd5a098c83508d6c1
valchev0 <georgi.v@mail.com>
2023-08-26 07:38:58 +0100

    Change Tiles

---
+++ CSharpBasics/CSharpBasicsBook/8.2. Exam Preparation - Part II/ChangeTiles/ChangeTiles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChangeTiles
+{
+    internal class ChangeTiles
+    {
+        static void Main(string[] args)
+        {
+            double savedMoney = double.Parse(Console.ReadLine());
+            double widthFloor = double.Parse(Console.ReadLine());
+            double lengthFloor = double.Parse(Console.ReadLine());
+            double sideTriangle = double.Parse(Console.ReadLine());
+            double heightTriangle = double.Parse(Console.ReadLine());
+            double tilePrice = double.Parse(Console.ReadLine());
+            double builderPrice = double.Parse(Console.ReadLine());
+
+            double areaFloor = widthFloor * lengthFloor;
+            double areaTile = (sideTriangle * heightTriangle) / 2;
+            double tileAmount = areaFloor / areaTile;
+            double totalPrice = ((Math.Round(tileAmount) + 5) * tilePrice) + builderPrice;
+            double diffrence = Math.Abs(totalPrice - savedMoney);
+
+            if (totalPrice <= savedMoney)
+            {
+                Console.WriteLine("{0:f2} lv left.", diffrence);
+            }
+            else
+            {
+                Console.WriteLine("You'll need {0:f2} lv more.", diffrence);
+            }
+        }
+    }
+}
