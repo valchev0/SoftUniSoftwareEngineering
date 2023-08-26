9b065d9c873a266b74d317c0edb626ce1006cee5
valchev0 <georgi.v@mail.com>
2023-08-26 17:32:42 +0100

    Blank Receipt

---
+++ CSharpBasics/CSharpBasicsBook/10. Methods/BlankReceipt/BlankReceipt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BlankReceipt
+{
+    internal class BlankReceipt
+    {
+        static void Main(string[] args)
+        {
+            PrintReceiotHeader();
+        }
+
+        private static void PrintReceiotHeader()
+        {
+            Console.WriteLine("CASH RECEIPT\r\n------------------------------\r" +
+                            "\nCharged to____________________\r\nReceived by___________________\r" +
+                            "\n------------------------------\r" +
+                            "\n(c) SoftUni");
+        }
+    }
+}
