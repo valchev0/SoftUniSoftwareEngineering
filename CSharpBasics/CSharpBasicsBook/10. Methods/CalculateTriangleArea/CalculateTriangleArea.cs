691146c30e1cbf0de10a99f36f3bfe97ffe394a1
valchev0 <georgi.v@mail.com>
2023-08-27 04:23:01 +0100

    Calculate Triangle Area

---
+++ CSharpBasics/CSharpBasicsBook/10. Methods/CalculateTriangleArea/CalculateTriangleArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculateTriangleArea
+{
+    internal class CalculateTriangleArea
+    {
+        static void Main(string[] args)
+        {
+            PrintArea();
+        }
+
+        private static void PrintArea()
+        {
+            int side = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int area = (side * height) / 2;
+            Console.WriteLine(area);
+        }
+    }
+}
