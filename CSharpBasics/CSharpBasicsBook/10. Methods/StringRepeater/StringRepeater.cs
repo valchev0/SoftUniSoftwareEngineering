1076eccd672c0a6db22cc6ff85198dfd5ee42d71
valchev0 <georgi.v@mail.com>
2023-08-27 13:58:13 +0100

    String Repeater

---
+++ CSharpBasics/CSharpBasicsBook/10. Methods/StringRepeater/StringRepeater.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringRepeater
+{
+    internal class StringRepeater
+    {
+        static void Main(string[] args)
+        {
+            StringRepeat();
+        }
+
+        private static void StringRepeat()
+        {
+            string str = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(str);
+            }
+        }
+    }
+}
