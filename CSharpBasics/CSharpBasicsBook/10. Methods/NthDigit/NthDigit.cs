b45da8ccf40f0dbe3a59af9582a91525f92464c9
valchev0 <georgi.v@mail.com>
2023-08-27 14:16:38 +0100

    Nth Digit

---
+++ CSharpBasics/CSharpBasicsBook/10. Methods/NthDigit/NthDigit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NthDigit
+{
+    internal class NthDigit
+    {
+        static void Main(string[] args)
+        {
+            DigitFinder();
+        }
+
+        private static void DigitFinder()
+        {
+            string number = Console.ReadLine();
+            string oposite = string.Empty;
+            int digit = int.Parse(Console.ReadLine());
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                oposite += number[i];
+            }
+            for (int i = 0; i < oposite.Length; i++)
+            {
+                if (digit - 1 == i)
+                {
+                    Console.WriteLine(oposite[i]);
+                }
+            }
+        }
+    }
+}
