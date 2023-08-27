ac80f73485671128007e01e43a6f4225c933c48b
valchev0 <georgi.v@mail.com>
2023-08-27 13:50:35 +0100

    Min Method

---
+++ CSharpBasics/CSharpBasicsBook/10. Methods/MinMethod/MinMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MinMethod
+{
+    internal class MinMethod
+    {
+        static void Main(string[] args)
+        {
+            GetMin();
+        }
+
+        private static void GetMin()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine((Math.Min(Math.Min(a, b), c)));
+        }
+    }
+}
