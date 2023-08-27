dbd9fb512281fcb57d5f757612b5e80f7132e1ac
valchev0 <georgi.v@mail.com>
2023-08-27 04:13:58 +0100

    Draw a Filled Square

---
+++ CSharpBasics/CSharpBasicsBook/10. Methods/DrawFilledSquare/DrawFilledSquare.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DrawFilledSquare
+{
+    internal class DrawFilledSquare
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char slash = (char)92;
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int cols = 1; cols <= 2 * n; cols++)
+                {
+                    if ((rows == 1 || rows == n) || (cols == 1 || cols == 2* n))
+                    {
+                        Console.Write('-');
+                    }
+                    else
+                    {
+                        if (cols % 2 == 0)
+                        {
+                            Console.Write(slash);
+                        }
+                        else
+                        {
+                            Console.Write('/');
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
