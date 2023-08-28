40a6be6c55571dd56499d6d78bfdd0a31489f170
valchev0 <georgi.v@mail.com>
2023-08-28 15:00:21 +0100

    Sum

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/Sum/Sum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sum
+{
+    internal class Sum
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            string consoleInput = Console.ReadLine();
+            int n = int.Parse(consoleInput);
+            int number = 1;
+            int sum = 1;
+
+            Console.Write("The sum 1");
+            while (number < n)
+            {
+                number++;
+                sum += number;
+                Console.Write("+{0}", number);
+            }
+            Console.WriteLine(" = {0}", sum);
+        }
+    }
+}
