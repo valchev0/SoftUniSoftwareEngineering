191695e65faf4886a7e3fd8335130a5d9d26f82f
valchev0 <georgi.v@mail.com>
2023-08-28 14:43:25 +0100

    Foreach

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/Foreach/Foreach.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Foreach
+{
+    internal class Foreach
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[] {
+            "Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday", "Sunday"
+        };
+            foreach (String day in days)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
