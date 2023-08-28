caf83957403c74a64af1474404603888ce75fee1
valchev0 <georgi.v@mail.com>
2023-08-28 15:13:28 +0100

    TwentyElementsPowFive

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/TwentyElementsPowFive/TwentyElementsPowFive.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TwentyElementsPowFive
+{
+    internal class TwentyElementsPowFive
+    {
+        static void Main(string[] args)
+        {
+            int n = 21;
+            int[] arr = new int[n];
+            for (int i = 1; i < n; i++)
+            {
+                arr[i] = i * 5;
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
