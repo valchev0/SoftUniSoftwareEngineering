5a45fe1d2ba99d1235d7277d9e362cb379b3b9fb
valchev0 <georgi.v@mail.com>
2023-08-28 15:03:07 +0100

    Triangle

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/Triangle/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Triangle
+{
+    internal class Triangle
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            string consoleInput = Console.ReadLine();
+            int n = int.Parse(consoleInput);
+            Console.WriteLine();
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int column = 1; column <= row; column++)
+                {
+                    Console.Write("{0} ", column);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
