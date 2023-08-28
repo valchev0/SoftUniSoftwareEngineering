2462452b6de160d853c43c1dd9c9cc5437e6c7ad
valchev0 <georgi.v@mail.com>
2023-08-28 14:37:43 +0100

    Factorial

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/Factorial/Factorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Factorial
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            string consoleInputLine = Console.ReadLine();
+            int n = Convert.ToInt32(consoleInputLine);
+
+            Console.Write("n! = ");
+
+            // "decimal" is the biggest integer type
+            decimal factorial = 1;
+
+            // Perform an infinite loop
+            while (true)
+            {
+                Console.Write(n);
+                if (n == 1)
+                {
+                    break;
+                }
+                Console.Write(" * ");
+                factorial *= n;
+                n--;
+            }
+            Console.WriteLine(" = {0}", factorial);
+        }
+    }
+}
