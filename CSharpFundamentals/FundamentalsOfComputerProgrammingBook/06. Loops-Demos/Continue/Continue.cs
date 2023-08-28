e8749de57688f6b09baa059b8d32c2a7fb47fe9a
valchev0 <georgi.v@mail.com>
2023-08-28 14:09:20 +0100

    Continue

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/Continue/Continue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Continue
+{
+    internal class Continue
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            string numberAsString = Console.ReadLine();
+            int n = int.Parse(numberAsString);
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; i += 2)
+            {
+                if ((i % 7) == 0)
+                    continue;
+                sum += i;
+            }
+            Console.WriteLine("sum = {0}", sum);
+        }
+    }
+}
