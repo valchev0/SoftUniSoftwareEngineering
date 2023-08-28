9adc0e65aa1a8c8116b32ed9478f245149c0f66c
valchev0 <georgi.v@mail.com>
2023-08-28 14:53:44 +0100

    IsPrime

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/IsPrime/IsPrime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IsPrime
+{
+    internal class IsPrime
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer number: ");
+            string numberAsString = Console.ReadLine();
+            uint number = uint.Parse(numberAsString);
+            uint divider = 2;
+            uint maxDivider = (uint)Math.Sqrt(number);
+            bool prime = true;
+            while (prime && (divider <= maxDivider))
+            {
+                if (number % divider == 0)
+                {
+                    prime = false;
+                }
+                divider++;
+            }
+            Console.WriteLine("Prime? {0}", prime);
+        }
+    }
+}
