3031b8da169089ec9fb6bbd0aa439a3b169beb7b
valchev0 <georgi.v@mail.com>
2023-08-28 14:55:19 +0100

    Power

--- CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/IsPrime/IsPrime.cs
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/IsPrime/IsPrime.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace IsPrime
+namespace Power
 {
     internal class IsPrime
     {
---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/Power/Power.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Power
+{
+    internal class Power
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            string numberAsString = Console.ReadLine();
+            int n = int.Parse(numberAsString);
+            Console.Write("m = ");
+            numberAsString = Console.ReadLine();
+            int m = int.Parse(numberAsString);
+
+            decimal result = 1;
+
+            for (int i = 0; i < m; i++)
+            {
+                result *= n;
+            }
+
+            Console.WriteLine("n^m = " + result);
+        }
+    }
+}
