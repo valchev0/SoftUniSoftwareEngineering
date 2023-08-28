d5b70cc7aec08f3fa3e2f819f0b3d637b6680798
valchev0 <georgi.v@mail.com>
2023-08-28 14:41:55 +0100

    Factorial

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/06. Loops-Demos/FactorialDoWhile/FactorialDoWhile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Factorial
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            string consoleInput = Console.ReadLine();
+            int n = Convert.ToInt32(consoleInput);
+            decimal factorial = 1;
+            //BigInteger factorial = 1;
+
+            do
+            {
+                factorial *= n;
+                n--;
+            }
+            while (n > 0);
+
+            Console.WriteLine("n! = " + factorial);
+        }
+    }
+}
