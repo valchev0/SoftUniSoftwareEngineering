3c64aba7845f76eb67c572a04702b3243a0e9446
valchev0 <georgi.v@mail.com>
2023-09-15 09:16:59 +0100

    Multiplication Table

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Lab/MultiplicationTable/MultiplicationTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class MultiplicationTable
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", n, i, (n * i));
+            }
+        }
+    }
+}
