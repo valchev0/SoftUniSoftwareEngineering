77b76a67c39e2aaaee43924124a26225460e0301
valchev0 <georgi.v@mail.com>
2023-09-15 08:13:50 +0100

    Back In 30 Minutes

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Lab/BackIn30Minutes/BackIn30Minutes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BackIn30Minutes
+{
+    internal class BackIn30Minutes
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine()) + 30;
+
+            if (minutes > 60)
+            {
+                minutes -= 60;
+                hours++;
+            }
+            if (hours > 23)
+            {
+                hours -= 24;
+            }
+            Console.WriteLine("{0:0}:{1:00}", hours, minutes);
+        }
+    }
+}
