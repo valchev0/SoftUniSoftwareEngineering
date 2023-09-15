b1f967ef0ca87b8bac75fed9c482c42549574fbb
valchev0 <georgi.v@mail.com>
2023-09-15 09:08:05 +0100

    um of Odd Numbers

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Lab/SumOfOddNumbers/SumOfOddNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    internal class SumOfOddNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int index = 1;
+            int sum = 0;
+            while (counter < n)
+            {
+                sum += index;
+                Console.WriteLine(index);
+                index += 2;
+                counter++;
+            }
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
