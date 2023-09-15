4b4efeeb24e8b414ce6bfea107627eb7e6bc6d02
valchev0 <georgi.v@mail.com>
2023-09-15 16:20:35 +0100

    Ages

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Exercice/Ages/Ages.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ages
+{
+    internal class Ages
+    {
+        static void Main(string[] args)
+        {
+            string inputAge = Console.ReadLine();
+            int age;
+
+            if (int.TryParse(inputAge, out age))
+            {
+                if (age <= 2)
+                {
+                    Console.WriteLine("baby");
+                }
+                else if (age <= 13)
+                {
+                    Console.WriteLine("child");
+                }
+                else if (age <= 19)
+                {
+                    Console.WriteLine("teenager");
+                }
+                else if (age <= 65)
+                {
+                    Console.WriteLine("adult");
+                }
+                else
+                {
+                    Console.WriteLine("elder");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid age input");
+            }
+        }
+    }
+}
