1a80970bc172e916338417d424c1585505821835
valchev0 <georgi.v@mail.com>
2023-09-15 10:18:47 +0100

    Even Number

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Lab/EvenNumber/EvenNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    internal class EvenNumber
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (true)
+            {
+                n = Math.Abs(int.Parse(Console.ReadLine()));
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("The number is: {0}", n);
+                    break;
+                }
+                else 
+                { 
+                    Console.WriteLine("Please write an even number."); 
+                }
+            }
+        }
+    }
+}
