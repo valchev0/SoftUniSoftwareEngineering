f883234a90c5590f1152208706a153602e71b18c
valchev0 <georgi.v@mail.com>
2023-09-15 17:21:03 +0100

    Login

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Exercice/Login/Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Login
+{
+    internal class Login
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                char chr = username[i];
+                password += chr;
+            }
+            string input;
+            int counter = 0;
+            bool isFound = false;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == password)
+                {
+                    isFound = true;
+                    break;
+                }
+                counter++;
+                if (counter == 4)
+                {
+                    Console.WriteLine("User {0} blocked!", username);
+                    return;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+            }
+            if (password == input || isFound == true) 
+            {
+                Console.WriteLine("User {0} logged in.", username);
+            }
+        }
+    }
+}
