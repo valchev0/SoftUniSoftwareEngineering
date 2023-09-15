19987c89d64a516ff22ee5d9e3263577baf22934
valchev0 <georgi.v@mail.com>
2023-09-15 16:52:45 +0100

    Print and sum

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Exercice/PrintAndSum/PrintAndSum.cs
@@ -0,0 +1,20 @@
+﻿namespace PrintAndSum
+{
+    internal class PrintAndSum
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                Console.Write("{0} ", i);
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
