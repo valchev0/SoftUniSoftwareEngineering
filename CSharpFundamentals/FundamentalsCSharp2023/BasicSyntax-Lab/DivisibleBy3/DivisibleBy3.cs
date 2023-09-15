a3958be2eaa7d9decf035a8fd60a09ea3bdd43da
valchev0 <georgi.v@mail.com>
2023-09-15 08:57:21 +0100

    Divisible by 3

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Lab/DivisibleBy3/DivisibleBy3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DivisibleBy3
+{
+    internal class DivisibleBy3
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i <= 99; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
