fc2ab8c08852ee575c2bdd42c41d536ef44cf7b5
valchev0 <georgi.v@mail.com>
2023-09-15 08:52:16 +0100

    Theatre Promotion

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Lab/TheatrePromotion/TheatrePromotion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace TheatrePromotion
+{
+    internal class TheatrePromotion
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+            bool isError = false;
+
+            if (age >= 0 && age <= 18)
+            {
+                switch (day)
+                {
+                    case "Weekday":
+                        price = 12;
+                        break;
+                    case "Weekend":
+                        price = 15;
+                        break;
+                    case "Holiday":
+                        price = 5;
+                        break;
+                    default: isError = true;
+                        break;
+                }
+            }
+            else if (age > 18 && age <= 64)
+            {
+                switch (day)
+                {
+                    case "Weekday":
+                        price = 18;
+                        break;
+                    case "Weekend":
+                        price = 20;
+                        break;
+                    case "Holiday":
+                        price = 12;
+                        break;
+                        default : isError = true;
+                        break;
+                }
+            }
+            else if (age > 64 && age <= 122)
+            {
+                switch (day)
+                {
+                    case "Weekday":
+                        price = 12;
+                        break;
+                    case "Weekend":
+                        price = 15;
+                        break;
+                    case "Holiday":
+                        price = 10;
+                        break;
+                        default: isError =true; 
+                        break;
+                }
+            }
+            else
+            {
+                isError = true;
+            }
+            if (!isError)
+            {
+                Console.WriteLine("{0}$", price);
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
