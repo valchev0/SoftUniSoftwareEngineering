e6e0f434bdc815b9758b6474759f45dc7e314dc7
valchev0 <georgi.v@mail.com>
2023-09-15 08:34:05 +0100

    Foreign Languages

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Lab/ForeignLanguages/ForeignLanguages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ForeignLanguages
+{
+    internal class ForeignLanguages
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+            switch (country)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Mexico":
+                case "Argentina":
+                    Console.WriteLine("Spanish");
+                    break;
+                    default: Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
