1a6002bee3ba5deda3866e7645ebfa34362b41d1
valchev0 <georgi.v@mail.com>
2023-09-15 09:24:20 +0100

    MultiplicationTable2

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Lab/MultiplicationTable2.0/MultiplicationTable2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultiplicationTable2
+{
+    internal class MultiplicationTable2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int startIndex = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{n} X {startIndex} = {n * startIndex}");
+                startIndex++;
+            }
+            while (startIndex <= 10);
+        }
+    }
+}
