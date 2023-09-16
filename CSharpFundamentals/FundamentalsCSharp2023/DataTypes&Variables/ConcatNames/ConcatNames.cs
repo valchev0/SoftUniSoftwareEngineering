31eb530dd7e6668b8787054bb7b092f6b66bd65a
valchev0 <georgi.v@mail.com>
2023-09-16 18:32:37 +0100

    Concat Names

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables/ConcatNames/ConcatNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConcatNames
+{
+    internal class ConcatNames
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+            Console.WriteLine("{0}{1}{2}", name, delimiter, surname);
+        }
+    }
+}
