736d062072ecdf7b9140667856dcd55608726090
valchev0 <georgi.v@mail.com>
2023-09-16 18:28:50 +0100

    Reversed Chars

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables/ReversedChars/ReversedChars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReversedChars
+{
+    internal class ReversedChars
+    {
+        static void Main(string[] args)
+        {
+            char chr = ' ';
+            string str = string.Empty;
+            string reverse = string.Empty;
+            for (int i = 0; i < 3; i++)
+            {
+                chr = char.Parse(Console.ReadLine());
+                str += ' ';
+                str += chr;
+            }
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverse += str[i];
+            }
+            Console.WriteLine("{0}", reverse);
+        }
+    }
+}
