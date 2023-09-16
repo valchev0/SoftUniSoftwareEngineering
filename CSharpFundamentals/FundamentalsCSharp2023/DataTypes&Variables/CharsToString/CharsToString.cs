7eaaa5c1b79f4be2c31085738be806a60d0cb561
valchev0 <georgi.v@mail.com>
2023-09-16 18:40:53 +0100

    Chars to String

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables/CharsToString/CharsToString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CharsToString
+{
+    internal class CharsToString
+    {
+        static void Main(string[] args)
+        {
+            string str = string.Empty;
+            for (int i = 0; i < 3; i++)
+            {
+                char chr = char.Parse(Console.ReadLine());
+                str += chr;
+            }
+            Console.WriteLine(str);
+
+        }
+    }
+}
