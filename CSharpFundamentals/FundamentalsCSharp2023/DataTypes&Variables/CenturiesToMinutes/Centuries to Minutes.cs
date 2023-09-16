6406de7ca25b62df2db09c2b6aad9ab97eb32d2e
valchev0 <georgi.v@mail.com>
2023-09-16 17:51:09 +0100

    Centuries to Minutes

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables/CenturiesToMinutes/Centuries to Minutes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace CenturiesToMinutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422);
+            BigInteger hours = days * 24;
+            BigInteger minutes = hours * 60;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
