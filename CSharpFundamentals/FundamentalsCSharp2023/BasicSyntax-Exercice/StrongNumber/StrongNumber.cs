3576107a4f72bbfec7cce3541df87202c22ae888
valchev0 <georgi.v@mail.com>
2023-09-16 07:15:09 +0100

    Strong number

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Exercice/StrongNumber/StrongNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace StrongNumber
+{
+    internal class StrongNumber
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int numCopy = n;
+            int lastDigit = 0;
+            int factorial;
+            int factorialSum = 0;
+
+            while (n > 0)
+            {
+                lastDigit = n % 10;
+                factorial = 1;
+                if (lastDigit > 1)
+                {
+                    for (int i = 1; i <= lastDigit; i++)
+                    {
+                        factorial *= i;
+                    }
+                }
+                factorialSum += factorial;
+                n /= 10;
+            }
+            if (factorialSum == numCopy)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
