030a8c1de6dcdebb6b4f9a03a5969921f88cb3f4
valchev0 <georgi.v@mail.com>
2023-09-16 11:27:48 +0100

    Reverse String

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-MoreExercice/ReverseString/ReverseString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReverseString
+{
+    internal class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string reverse = string.Empty;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reverse += input[i];
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
