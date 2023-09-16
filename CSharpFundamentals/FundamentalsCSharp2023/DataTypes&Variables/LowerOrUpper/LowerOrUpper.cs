0157c484933adf32398d470a21aa9784815d603b
valchev0 <georgi.v@mail.com>
2023-09-16 18:50:11 +0100

    Lower or Upper

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables/LowerOrUpper/LowerOrUpper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LowerOrUpper
+{
+    internal class LowerOrUpper
+    {
+        static void Main(string[] args)
+        {
+            char input = Console.ReadLine()[0];
+            if (char.IsUpper(input))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
