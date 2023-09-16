5721c4ebaf5acf9ddc4c875e1d9f85a489d203b8
valchev0 <georgi.v@mail.com>
2023-09-16 17:37:02 +0100

    Pounds to Dollars

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables/PoundsToDollars/PoundsToDollars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PoundsToDollars
+{
+    internal class PoundsToDollars
+    {
+        static void Main(string[] args)
+        {
+            decimal input = decimal.Parse(Console.ReadLine());
+            decimal output = input * 1.31m;
+            Console.WriteLine("{0:0.000}", output);
+        }
+    }
+}
