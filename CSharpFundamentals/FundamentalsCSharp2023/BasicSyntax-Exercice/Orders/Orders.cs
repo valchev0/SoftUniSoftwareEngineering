88ab8a18463792904f96ad0533d0d396b10f9a77
valchev0 <georgi.v@mail.com>
2023-09-16 09:27:20 +0100

    Orders

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Exercice/Orders/Orders.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Orders
+{
+    internal class Orders
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double coffeePrice = 0;
+            double sumTotal = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double priceCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsules = int.Parse(Console.ReadLine());
+                coffeePrice = (days * capsules) *priceCapsule;
+                Console.WriteLine("The price for the coffee is: ${0:0.00}", coffeePrice);
+                sumTotal += coffeePrice;
+            }
+            Console.WriteLine("Total: ${0:0.00}", sumTotal);
+        }
+    }
+}
