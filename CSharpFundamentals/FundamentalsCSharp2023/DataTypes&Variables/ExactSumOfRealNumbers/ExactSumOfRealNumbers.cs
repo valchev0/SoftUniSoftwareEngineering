1d31c54f5097ac055e51a27a50dc371b23efab0f
valchev0 <georgi.v@mail.com>
2023-09-16 17:40:30 +0100

    Exact Sum of Real Numbers

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables/ExactSumOfRealNumbers/ExactSumOfRealNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExactSumOfRealNumbers
+{
+    internal class ExactSumOfRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            decimal input = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                input = decimal.Parse(Console.ReadLine());
+                sum += input;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
