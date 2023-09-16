578f304f96b2cb4ee43203c6d8360c5c23259cf6
valchev0 <georgi.v@mail.com>
2023-09-16 17:33:47 +0100

    Convert Meters to Kilometers

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables/ConvertMetersToKilometers/ConvertMetersToKilometers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConvertMetersToKilometers
+{
+    internal class ConvertMetersToKilometers
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+            double output = input / 1000;
+            Console.WriteLine("{0:0.00}", output);
+        }
+    }
+}
