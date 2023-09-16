e35f0205d4e396585298b79cc8fd641de0a5d625
valchev0 <georgi.v@mail.com>
2023-09-16 07:52:58 +0100

    Triangle of Numbers

---
+++ CSharpFundamentals/FundamentalsCSharp2023/BasicSyntax-Exercice/TriangleOfNumbers/TriangleOfNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TriangleOfNumbers
+{
+    internal class TriangleOfNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int index = 0;
+            for (int rows = 0; rows <= n; rows++)
+            {
+                for (int cows = 1; cows <= rows; cows++)
+                {
+                    Console.Write(index + " ");
+                }
+                index++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
