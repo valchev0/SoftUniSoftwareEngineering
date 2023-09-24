6c5a63662e94efa895af64b5eb7de1b4a2bfd7e4
valchev0 <georgi.v@mail.com>
2023-09-24 16:56:25 +0100

    Refactor Special Numbers

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables/RefactorSpecialNumbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,31 @@
+﻿namespace RefactorSpecialNumbers
+{
+    internal class RefactorSpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNum = i;
+                int lastDigit = 0;
+                int sum = 0;
+                while (currentNum > 0)
+                {
+                    lastDigit = currentNum % 10;
+                    sum += lastDigit;
+                    currentNum /= 10;
+                }
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine("{0} -> True", i);
+                }
+                else
+                {
+                    Console.WriteLine("{0} -> False", i);
+                }
+            }
+        }
+    }
+}
