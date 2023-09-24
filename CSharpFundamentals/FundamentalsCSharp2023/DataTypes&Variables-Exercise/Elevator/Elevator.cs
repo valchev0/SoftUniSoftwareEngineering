40439ff54c649bb3acd3cb051a8d48cc775f0b41
valchev0 <georgi.v@mail.com>
2023-09-24 17:31:53 +0100

    Elevator

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables-Exercise/Elevator/Elevator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Elevator
+{
+    internal class Elevator
+    {
+        static void Main(string[] args)
+        {
+            double people = double.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int cources = 0;
+
+            Console.WriteLine(cources = (int)Math.Ceiling(people / capacity)) ;
+        }
+    }
+}
