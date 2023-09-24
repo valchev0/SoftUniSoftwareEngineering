3d3d1c82e4432cf5346297b2ab5478fc995b1d9c
valchev0 <georgi.v@mail.com>
2023-09-24 17:24:35 +0100

    Sum Digits

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables-Exercise/SumDigits/SumDigits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumDigits
+{
+    internal class SumDigits
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int lastDigit = 0;
+            int sum = 0;
+
+            while (n > 0)
+            {
+                lastDigit = n % 10;
+                sum += lastDigit;
+                n /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
