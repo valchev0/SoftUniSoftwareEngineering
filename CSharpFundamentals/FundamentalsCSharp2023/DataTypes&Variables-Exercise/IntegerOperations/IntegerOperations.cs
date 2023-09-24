d44f7a5195045223b9c0c9a8854bbfc50b1368b3
valchev0 <georgi.v@mail.com>
2023-09-24 17:20:14 +0100

    Integer Operations

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables-Exercise/IntegerOperations/IntegerOperations.cs
@@ -0,0 +1,18 @@
+﻿namespace IntegerOperations
+{
+    internal class IntegerOperations
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int forthNum = int.Parse(Console.ReadLine());
+
+            int sum = firstNum + secondNum;
+            sum = sum / thirdNum;
+            sum = sum * forthNum;
+            Console.WriteLine(sum);
+        }
+    }
+}
