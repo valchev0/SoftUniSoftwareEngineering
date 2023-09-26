7332f15e8b1b10fb72e2fbb6660ebdd5d7dc75a9
valchev0 <georgi.v@mail.com>
2023-09-26 15:24:45 +0100

    Sum of Chars

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables-Exercise/SumOfChars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumOfChars
+{
+    internal class SumOfChars
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            char chr = ' ';
+
+            for (int i = 0; i < n; i++)
+            {
+                chr = char.Parse(Console.ReadLine());
+                sum += chr;
+            }
+            Console.WriteLine("The sum equals: {0}", sum);
+        }
+    }
+}
