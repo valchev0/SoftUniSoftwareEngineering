e3896a1e13dba3776de95a49d7ef937dba83e604
valchev0 <georgi.v@mail.com>
2023-12-09 16:40:48 +0000

    Snowballs

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables-Exercise/Snowballs/Snowballs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    internal class Snowballs
+    {
+        static void Main(string[] args)
+        {
+            int snowballs = int.Parse(Console.ReadLine());
+            BigInteger snowballSnow = 0;
+            BigInteger snowballTime = 0;
+            int snowballQuality = 0;
+            BigInteger bestSnowball = int.MinValue;
+            BigInteger snowballValue = 0;
+            string snowballFormula = string.Empty;
+
+            for (int i = 0; i < snowballs; i++)
+            {
+                snowballSnow = BigInteger.Parse(Console.ReadLine());
+                snowballTime = BigInteger.Parse(Console.ReadLine());
+                snowballQuality = int.Parse(Console.ReadLine());
+                BigInteger currentSnowballValue = snowballSnow / snowballTime;
+                snowballValue = BigInteger.Pow(currentSnowballValue, snowballQuality);
+                if (snowballValue > bestSnowball)
+                {
+                    bestSnowball = snowballValue;
+                    snowballFormula = $"{snowballSnow} : {snowballTime} = {snowballValue} ({snowballQuality})";
+                }
+            }
+            Console.WriteLine(snowballFormula);
+        }
+    }
+}
