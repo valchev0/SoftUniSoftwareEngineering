6b60e3db1e9f9e78a05d49f48aaa7c1f91ef056d
valchev0 <georgi.v@mail.com>
2023-12-09 16:38:51 +0000

    Poke Mon

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables-Exercise/PokeMon/PokeMon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PokeMon
+{
+    internal class PokeMon
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int startingPower = power;
+            int counter = 0;
+
+            while (power >= distance)
+            {
+                power -= distance;
+                counter++;
+                if (startingPower * 0.5 == power && exhaustionFactor > 0)
+                {
+                    power /= exhaustionFactor;
+                }
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(counter);
+        }
+    }
+}
