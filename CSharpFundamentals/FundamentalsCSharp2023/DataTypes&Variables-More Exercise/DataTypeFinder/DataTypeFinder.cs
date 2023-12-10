f3c84e01f8e31dc7ae5897527093a94bceacd733
valchev0 <georgi.v@mail.com>
2023-12-10 09:35:49 +0000

    02. From Left to The Right

---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables-More Exercise/DataTypeFinder/DataTypeFinder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DataTypeFinder
+{
+    internal class DataTypeFinder
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (int.TryParse(input, out int number))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (float.TryParse(input, out float point))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input, out char character))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (bool.TryParse(input, out bool value))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsCSharp2023/DataTypes&Variables-More Exercise/FromLeftToTheRight/FromLeftToTheRight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FromLeftToTheRight
+{
+    internal class FromLeftToTheRight
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < n; i++)
+            {
+                var array = Console.ReadLine()
+                    .Split(" ")
+                    .Select(long.Parse)
+                    .ToArray();
+
+                var leftNumber = array[0];
+                var rightNumber = array[1];
+
+                Console.WriteLine(leftNumber > rightNumber
+                    ? SumDigits(leftNumber)
+                    : SumDigits(rightNumber));
+            }
+        }
+        private static int SumDigits(long number)
+        {
+            var sum = 0;
+
+            while (number != 0)
+            {
+                sum += (int)(number % 10);
+                number /= 10;
+            }
+
+            return Math.Abs(sum);
+        }
+    }
+}
