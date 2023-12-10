bf62aa75e90e398dc05c794f902f9156ca3f3645
valchev0 <georgi.v@mail.com>
2023-12-10 09:45:58 +0000

    01. Day of Week

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays-Lab/DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    internal class DayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            string[] dayOfWeek = 
+            { 
+                "Monday", 
+                "Tuesday", 
+                "Wednesday", 
+                "Thursday", 
+                "Friday", 
+                "Saturday", 
+                "Sunday" 
+            };
+
+            int day = int.Parse(Console.ReadLine());
+
+            if (day > 0 && day <= 7)
+            {
+                Console.WriteLine(dayOfWeek[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
