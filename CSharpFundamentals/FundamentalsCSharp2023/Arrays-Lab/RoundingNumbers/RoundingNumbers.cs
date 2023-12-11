faea80f0f62c4fd824b0ce3f2bed697788725523
valchev0 <georgi.v@mail.com>
2023-12-11 16:21:05 +0000

    Rounding Numbers

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays-Lab/RoundingNumbers/RoundingNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RoundingNumbers
+{
+    internal class RoundingNumbers
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(double.Parse)
+            .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {((int)Math.Round(numbers[i], MidpointRounding.AwayFromZero))}");
+            }
+        }
+    }
+}
