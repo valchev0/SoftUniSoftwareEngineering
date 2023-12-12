50c7c823989b918d452d716e33eeb27d02793741
valchev0 <georgi.v@mail.com>
2023-12-12 02:39:29 +0000

    06. Even and Odd Subtraction

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays-Lab/Even&OddSubtraction/Even&OddSubtraction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Even_OddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int evenSum = 0;
+            int oddSum = 0;
+            int diffrence = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0) 
+                {
+                    evenSum += arr[i];
+                }
+                else
+                {
+                    oddSum += arr[i];
+                }
+            }
+            diffrence = evenSum - oddSum;
+            Console.WriteLine(diffrence);
+
+        }
+    }
+}
