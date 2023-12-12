cac9c20ec87d0d509ea49ad0c6cab73ef47c1d02
valchev0 <georgi.v@mail.com>
2023-12-12 02:53:42 +0000

    Equal Arrays

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays-Lab/EqualArrays/EqualArrays.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace EqualArrays
+{
+    internal class EqualArrays
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+            int[] secondArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            bool isDiffrent = false;
+            int sum = 0;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                {
+                    isDiffrent = true;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                sum += firstArray[i];
+
+                if (isDiffrent)
+                {
+                    break;
+                }
+            }
+
+            if (!isDiffrent)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+
+            //string firstArrayAsString = string.Join("", firstArray);
+            //string secondArrayAsString = string.Join("",  secondArray);
+
+            //for (int i = 0; i < firstArray.Length; i++)
+            //{
+            //    sum += firstArray[i];
+            //}
+
+            //if (firstArrayAsString == secondArrayAsString)
+            //{
+            //    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
