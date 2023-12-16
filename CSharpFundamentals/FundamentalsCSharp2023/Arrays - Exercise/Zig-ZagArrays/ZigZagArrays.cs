ead4e84c1a8dcb63f6f0128453368f77616ea6c2
valchev0 <georgi.v@mail.com>
2023-12-16 16:03:09 +0000

    03. Zig-Zag Arrays

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays - Exercise/Zig-ZagArrays/ZigZagArrays.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ZigZagArrays
+{
+    internal class ZigZagArrays
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            int[] firstArray = new int[n];
+            int[] secondArray = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                counter++;
+                int[] temporaryArray = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                if (counter % 2 == 1)
+                {
+                    firstArray[i] = temporaryArray[0];
+                    secondArray[i] = temporaryArray[1];
+
+                }
+                else
+                {
+                    secondArray[i] = temporaryArray[0];
+                    firstArray[i] = temporaryArray[1];
+                }
+            }
+            Console.WriteLine(string.Join(" ", firstArray));
+            Console.WriteLine(string.Join(" ", secondArray));
+        }
+    }
+}
