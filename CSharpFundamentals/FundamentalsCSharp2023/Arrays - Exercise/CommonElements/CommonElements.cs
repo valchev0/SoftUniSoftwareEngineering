bb52c643390c1f0337cc639c5622ae315acf43cd
valchev0 <georgi.v@mail.com>
2023-12-16 15:50:32 +0000

    02. Common Elements

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays - Exercise/CommonElements/CommonElements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace CommonElements
+{
+    internal class CommonElements
+    {
+        static void Main(string[] args)
+        {
+            string[] secondArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] firstArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] commonElements = new string[firstArray.Length];
+            
+            int counter = 0;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                for (int j = 0; j < secondArray.Length; j++)
+                {
+                    if (firstArray[i] == secondArray[j])
+                    {
+                        commonElements[counter] = firstArray[i];
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", commonElements));
+
+        }
+    }
+}
