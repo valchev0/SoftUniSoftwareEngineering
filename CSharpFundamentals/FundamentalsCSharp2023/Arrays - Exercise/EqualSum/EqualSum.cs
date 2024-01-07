0bdf6f054d35449ec8ce16d8f70a3309e9eac561
valchev0 <georgi.v@mail.com>
2024-01-07 18:33:41 +0000

    Equal Sum

--- CSharpFundamentals/FundamentalsCSharp2023/Arrays - Exercise/EqualSum/EqualSum.cs
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays - Exercise/EqualSum/EqualSum.cs
@@ -6,7 +6,37 @@ namespace EqualSum
     {
         static void Main(string[] args)
         {
-            
+            int[] array = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int leftSum = 0;
+            int rightSum = 0;
+            bool isFound = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    rightSum += array[j];
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    leftSum += array[k];
+                }
+                if (leftSum == rightSum)
+                {
+                    Console.Write("{0} ", i);
+                    isFound = true;
+                }
+                leftSum = 0;
+                rightSum = 0;
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("no");
+            }
         }
     }
 }
