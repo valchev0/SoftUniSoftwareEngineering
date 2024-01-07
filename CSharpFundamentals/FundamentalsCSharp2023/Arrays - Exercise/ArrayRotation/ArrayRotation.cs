78c8a6be191067fdf672cf80e6213eb768142e59
valchev0 <georgi.v@mail.com>
2024-01-07 06:57:53 +0000

    04. Array Rotation

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays - Exercise/ArrayRotation/ArrayRotation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayRotation
+{
+    internal class ArrayRotation
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                          .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                          .Select(int.Parse)
+                          .ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int firstElement = array[0];
+
+                int[] temp = new int[array.Length];
+
+                for (int j = 1; j < temp.Length; j++)
+                {
+                    temp[j - 1] = array[j];
+                }
+                temp[temp.Length - 1] = firstElement;
+                array = temp;
+            }
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays - Exercise/Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                          .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                          .Select(int.Parse)
+                          .ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            int[] temp = new int[array.Length];
+
+            for (
+                int i = 0; i < n; i++)
+            {
+                int firstElement = array[0];
+                for (int j = 1; j < temp.Length; j++)
+                {
+                    temp[j - 1] = array[j];
+                }
+                temp[temp.Length - 1] = firstElement;
+                array = temp;
+            }
+            Console.WriteLine(string.Join(" ", temp));
+        }
+    }
+}
