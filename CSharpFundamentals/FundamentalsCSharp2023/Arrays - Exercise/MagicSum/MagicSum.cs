716238260981e2581465ce42d1f8ef1581e6c691
valchev0 <georgi.v@mail.com>
2024-01-11 01:14:17 +0000

    08. Magic Sum

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays - Exercise/MagicSum/MagicSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MagicSum
+{
+    internal class MagicSum
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] != array[j])
+                    {
+                        if (array[i] + array[j] == sum)
+                        {
+                            Console.WriteLine(array[i] + " " + array[j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
