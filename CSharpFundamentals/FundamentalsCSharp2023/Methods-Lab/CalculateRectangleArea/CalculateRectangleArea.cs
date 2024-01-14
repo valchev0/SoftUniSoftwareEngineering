b2f631c01ed9af5660e313d6c2ab2d328483d1a2
valchev0 <georgi.v@mail.com>
2024-01-14 17:49:04 +0000

    06. Calculate Rectangle Area

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Lab/CalculateRectangleArea/CalculateRectangleArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculateRectangleArea
+{
+    internal class CalculateRectangleArea
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int result = RectangleArea(width, height);
+            Console.WriteLine(result);
+        }
+
+        private static int RectangleArea(int width, int height)
+        {
+            return width * height;
+        }
+    }
+}
