f9adda297366ee317ac698bf60213d153c608cfe
valchev0 <georgi.v@mail.com>
2024-01-14 17:40:03 +0000

    05. Orders

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Lab/Orders/Orders.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Orders
+{
+    internal class Orders
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            double price = 0;
+            double sum = 0;
+            sum = OrderSum(product, n, ref price);
+            Console.WriteLine(sum);
+        }
+
+        private static double OrderSum(string product, int n, ref double price)
+        {
+            double sum;
+            switch (product)
+            {
+                case "coffee":
+                    price = 1.5;
+                    break;
+                case "water":
+                    price = 1;
+                    break;
+                case "coke":
+                    price = 1.4;
+                    break;
+                case "snacks":
+                    price = 2;
+                    break;
+            }
+            sum = price * n;
+            return sum;
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Lab/PrintingTriangle/PrintingTriangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PrintingTriangle
+{
+    internal class PrintingTriangle
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Up(n);
+            Down(n);
+        }
+
+        private static void Down(int n)
+        {
+            for (int column = n - 1; column > 0; column--)
+            {
+                for (int row = 1; row <= column; row++)
+                {
+                    Console.Write(row + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void Up(int n)
+        {
+            for (int column = 1; column <= n; column++)
+            {
+                for (int row = 1; row <= column; row++)
+                {
+                    Console.Write(row + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
