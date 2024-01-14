0844f3a5ed25d4f25e195faa074703c6db036a83
valchev0 <georgi.v@mail.com>
2024-01-14 09:47:28 +0000

    01. Sign of Integer Numbers

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Arrays - Exercise/KaminoFactory/KaminoFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace KaminoFactory
+{
+    internal class KaminoFactory
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int dnaLength = int.Parse(input);
+            int[] array = new int[dnaLength];
+            int[] bestDNA = new int[dnaLength];
+            int counter = 0;
+            int dnaCounter = 1;
+            int bestDNACounter = 1;
+            int startIndex = 0;
+            int besStartIndex = 1;
+
+            while ((input = Console.ReadLine()) != "Clone them!")
+            {
+                counter++;
+                for (int i = 0; i < dnaLength; i++)
+                {
+                    array = input
+                        .Split('!', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+                    for (int j = 1; j < dnaLength; j++)
+                    {
+                        if (array[j] == array[j - 1])
+                        {
+                            dnaCounter++;
+                        }
+                        else
+                        {
+                            dnaCounter = 1;
+                        }
+                        if (j - 1 < besStartIndex && dnaCounter > bestDNACounter)
+                        {
+                            bestDNACounter = dnaCounter;
+                            //startIndex = j - 1;
+                            bestDNA = array.ToArray();
+                            besStartIndex = j - 1;
+                        }
+                    }
+                    //sum = array.Sum();
+                    //if (sum > bestSum)
+                    //{
+                    //    bestSum = sum;
+                    //    bestDNA = array;
+                    //}
+                }
+            }
+            Console.WriteLine("Best DNA sample {0} with sum: {1}.", counter, bestDNA.Sum());
+            Console.WriteLine(string.Join(" ", bestDNA));
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Lab/SignOfIntegerNumbers/SignOfIntegerNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SignOfIntegerNumbers
+{
+    internal class SignOfIntegerNumbers
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string output = string.Empty;
+            output = SignInteger(input);
+            Console.WriteLine("The number {0} is {1}.", input, output);
+        }
+
+        private static string SignInteger(int input)
+        {
+            string output;
+            if (input > 0)
+            {
+                output = "positive";
+            }
+            else if (input < 0)
+            {
+                output = "negative";
+            }
+            else
+            {
+                output = "zero";
+            }
+
+            return output;
+        }
+    }
+}
