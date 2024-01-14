7ca94cf46442fd2c08db107365ecf832a9a548ff
valchev0 <georgi.v@mail.com>
2024-01-14 16:52:26 +0000

    03. Calculations

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Lab/Calculations/Calculations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calculations
+{
+    internal class Calculations
+    {
+        static void Main(string[] args)
+        {
+            string action = Console.ReadLine();
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int result = 0;
+            result = CalculationResult(action, firstNum, secondNum, result);
+
+            Console.WriteLine(result);
+        }
+
+        private static int CalculationResult(string action, int firstNum, int secondNum, int result)
+        {
+            switch (action)
+            {
+                case "add":
+                    result = firstNum + secondNum;
+                    break;
+                case "multiply":
+                    result = firstNum * secondNum;
+                    break;
+                case "subtract":
+                    result = firstNum - secondNum;
+                    break;
+                case "divide":
+                    result = firstNum / secondNum;
+                    break;
+            }
+            return result;
+        }
+    }
+}
