1418cc42005ebd00ad373f708bb9abcbde66188c
valchev0 <georgi.v@mail.com>
2024-01-15 15:38:06 +0000

    08. Math Power

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Lab/MathPower/MathPower.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MathPower
+{
+    internal class MathPower
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+            double result = MatPower(firstNum, secondNum);
+            Console.WriteLine(result);
+        }
+
+        private static double MatPower(double firstNum, double secondNum)
+        {
+            double multiplier = Math.Pow(firstNum, secondNum);
+            return multiplier;
+        }
+    }
+}
