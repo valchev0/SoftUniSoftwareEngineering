966bd0ad36f80d76c5a8470c56213642229f9b7f
valchev0 <georgi.v@mail.com>
2024-01-16 15:33:49 +0000

    10. Multiply Evens by Odds

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Lab/MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MultiplyEvensByOdds
+{
+    internal class MultiplyEvensByOdds
+    {
+        static void Main(string[] args)
+        {
+            int input = Math.Abs(int.Parse(Console.ReadLine()));
+            input = FinalResult(input);
+        }
+
+        private static int FinalResult(int input)
+        {
+            int lastDigit = 0;
+            int evenSum = 0;
+            int oddSum = 0;
+            int result = 0;
+            while (input > 0)
+            {
+                lastDigit = input % 10;
+                if (lastDigit % 2 == 0)
+                {
+                    evenSum += lastDigit;
+                }
+                else
+                {
+                    oddSum += lastDigit;
+                }
+                input /= 10;
+            }
+            result = evenSum * oddSum;
+            Console.WriteLine(result);
+            return input;
+        }
+    }
+}
