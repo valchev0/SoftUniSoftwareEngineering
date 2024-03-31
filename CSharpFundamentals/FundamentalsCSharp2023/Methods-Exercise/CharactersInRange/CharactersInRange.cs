e89dc653c98e6b16f86b34a582d345812748d0c9
valchev0 <georgi.v@mail.com>
2024-03-31 14:45:36 +0100

    Characters in Range

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Exercise/CharactersInRange/CharactersInRange.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CharactersInRange
+{
+    internal class CharactersInRange
+    {
+        static void Main(string[] args)
+        {
+            char charStart = char.Parse(Console.ReadLine());
+            char charEnd = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Join(" ", (CharactersArray(charStart, charEnd))));
+        }
+
+        static char[] CharactersArray(char charStart, char charEnd)
+        {
+            //int length = Math.Abs(start.CompareTo(end));
+            //char[] result = new char[length];
+            //int counter = 0;
+            //for (int i = (int)start + 1; i < (int)end; i++)
+            //{
+            //    char[counter] = ((int)start) 
+            //}
+
+            int length = Math.Abs(charStart.CompareTo(charEnd));
+            char[] chars = new char[length];
+
+            int start = (int)charStart + 1;
+            int end = (int)charEnd;
+            int counter = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                chars[counter] = (char)i;
+                counter++;
+            }
+            return chars;
+        }
+    }
+}
