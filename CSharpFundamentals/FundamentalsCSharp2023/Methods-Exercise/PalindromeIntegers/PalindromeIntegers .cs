2c01d90ecaa28f95d264e8bf30143d8f40a3dcec
valchev0 <georgi.v@mail.com>
2024-04-01 18:06:42 +0100

    09. Palindrome Integers

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Exercise/PalindromeIntegers/PalindromeIntegers .cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            PolidromeIdentifier(input);
+        }
+        
+        static string PolidromeIdentifier (string input)
+        {
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input[0] == input[input.Length - 1])
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+            return input;
+        }
+    }
+}
