3445ffcf0c60c9d80e4d1f1c0264715562ba5c3e
valchev0 <georgi.v@mail.com>
2024-04-01 13:42:16 +0100

    08. Factorial Division

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Methods-Exercise/FactorialDivision/FactorialDivision.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FactorialDivision
+{
+    internal class FactorialDivision
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{FactorialDivisionResult(firstNum, secondNum):f2}");
+        }
+        static decimal FactorialDivisionResult (int firstNum, int secondNum)
+        {
+
+            decimal firstFactorial = 1;
+            for (int i = 1; i <= firstNum; i++)
+            {
+                firstFactorial *= i;
+            }
+
+            decimal secondFactorial = 1;
+            for(int i = 1; i <= secondNum; i++)
+            {
+                secondFactorial *= i;
+            }
+
+            decimal result = firstFactorial / secondFactorial;
+            return result;
+        }
+    }
+}
