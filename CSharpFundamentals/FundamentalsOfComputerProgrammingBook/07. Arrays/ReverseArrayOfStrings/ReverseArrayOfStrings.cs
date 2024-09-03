1c5322de9cc7c8311f2864e821f19da313291b06
valchev0 <georgi.v@mail.com>
2024-09-03 16:59:33 +0100

    Reverse Array of Strings

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/ReverseArrayOfStrings/ReverseArrayOfStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArrayOfStrings
+{
+    internal class ReverseArrayOfStrings
+    {
+        static void Main(string[] args)
+        {
+            //string[] arr = Console.ReadLine()
+            //    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            //    .Reverse()
+            //    .ToArray();
+
+            string[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                var temp = arr[arr.Length - i - 1];
+                arr[arr.Length - i - 1] = arr[i];
+                arr[i] = temp;
+            }
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
