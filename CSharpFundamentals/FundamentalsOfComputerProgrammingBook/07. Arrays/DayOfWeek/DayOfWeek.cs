0ac11f50103d5b7333493a12bd0d1b5af5e360b2
valchev0 <georgi.v@mail.com>
2024-09-03 15:33:42 +0100

    Day of Week

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    internal class DayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            string[] arr =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            int n = int.Parse(Console.ReadLine()) - 1;
+
+            if (n >= 0 && n <= 6)
+            {
+                Console.WriteLine(arr[n]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/TripleSum/TripleSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace TripleSum
+
+{
+    internal class TripleSum
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            bool isFound = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int sum = arr[i] + arr[j];
+                    if (arr.Contains(sum))
+                    {
+                        Console.WriteLine("{0} + {1} == {2}", arr[i], arr[j], sum);
+                        isFound = true;
+                    }
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
