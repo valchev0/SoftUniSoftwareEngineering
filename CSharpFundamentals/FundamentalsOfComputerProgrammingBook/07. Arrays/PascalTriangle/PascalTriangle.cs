1da98518d7f79a6fb2747d56ff057321d8611202
valchev0 <georgi.v@mail.com>
2024-09-04 17:35:23 +0100

    Pascal Triangle

--- CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/PascalTriangle/PascalTriangle.cs
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/PascalTriangle/PascalTriangle.cs
@@ -1,74 +1,31 @@
 ﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Numerics;
 
 namespace PascalTriangle
 {
-    internal class PascalTriangle
+    class Program
     {
         static void Main(string[] args)
         {
-            const int HEIGHT = 12;
+            int height = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[height][];
 
-
-
-            // Allocate the array in a triangle form
-
-            long[][] triangle = new long[HEIGHT + 1][];
-
-
-
-            for (int row = 0; row < HEIGHT; row++)
-
+            for (int i = 0; i < height; i++)
             {
+                long[] row = new long[i + 1];
+                row[0] = 1;
+                row[i] = 1;
 
-                triangle[row] = new long[row + 1];
-
-            }
-
-
-
-            // Calculate the Pascal's triangle
-
-            triangle[0][0] = 1;
-
-            for (int row = 0; row < HEIGHT - 1; row++)
-
-            {
-
-                for (int col = 0; col <= row; col++)
-
+                for (int j = 1; j < i; j++)
                 {
-
-                    triangle[row + 1][col] += triangle[row][col];
-
-                    triangle[row + 1][col + 1] += triangle[row][col];
-
+                    row[j] = triangle[i - 1][j] + triangle[i - 1][j - 1];
                 }
 
+                triangle[i] = row;
             }
 
-
-
-            // Print the Pascal's triangle
-
-            for (int row = 0; row < HEIGHT; row++)
-
+            for (int i = 0; i < height; i++)
             {
-
-                Console.Write("".PadLeft((HEIGHT - row) * 2));
-
-                for (int col = 0; col <= row; col++)
-
-                {
-
-                    Console.Write("{0,3} ", triangle[row][col]);
-
-                }
-
-                Console.WriteLine();
-
+                Console.WriteLine(string.Join(' ', triangle[i]));
             }
         }
     }
