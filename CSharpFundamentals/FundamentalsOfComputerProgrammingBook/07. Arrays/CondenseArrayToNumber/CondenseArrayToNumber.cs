fcd9eb334e5d115c4270298817ab9b2ced90b4ac
valchev0 <georgi.v@mail.com>
2024-09-04 11:22:49 +0100

    Condense Array to Number

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/CondenseArrayToNumber/CondenseArrayToNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+
+namespace CondenseArrayToNumber
+{
+    internal class CondenseArrayToNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            while (arr.Length > 1)
+            {
+                int[] condenced = new int[arr.Length - 1];
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    condenced[i] = arr[i] + arr[i + 1];
+                }
+                arr = condenced;
+            }
+            Console.WriteLine(arr[0]);
+        }
+    }
+}
