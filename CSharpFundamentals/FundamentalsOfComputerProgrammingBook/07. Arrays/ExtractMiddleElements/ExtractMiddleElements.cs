530d34642a8628980ca137ad64a6ee9039dcb4cf
valchev0 <georgi.v@mail.com>
2024-09-04 11:58:57 +0100

    Extract Middle 1, 2 or 3 Elements

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/ExtractMiddleElements/ExtractMiddleElements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+
+namespace ExtractMiddleElements
+{
+    internal class ExtractMiddleElements
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            string message = null;
+            if (arr.Length == 1)
+            {
+                message = string.Format("{{ {0} }}", arr[0]);
+            }
+            else if (arr.Length % 2 == 0)
+            {
+                int first = (arr.Length / 2) - 1;
+                int second = (arr.Length / 2);
+                message = string.Format("{{ {0}, {1} }}", arr[first], arr[second]);
+            }
+            else
+            {
+                int first = (arr.Length / 2) - 1;
+                int second = (arr.Length / 2);
+                int third = (arr.Length / 2) + 1;
+
+                message = string.Format("{{ {0}, {1}, {2} }}", arr[first], arr[second], arr[third]);
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
