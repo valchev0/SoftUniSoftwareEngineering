cf51eb406cfbe84d900da40de1e7fa39f886830d
valchev0 <georgi.v@mail.com>
2024-09-06 06:08:54 +0100

    Fibonacci

--- CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/Fibonachi/Fibonachi.cs
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/07. Arrays/Fibonachi/Fibonachi.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 
-namespace Fibonachi
+namespace Fibonacci
 {
-    internal class Fibonachi
+    internal class Fibonacci
     {
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int k = int.Parse(Console.ReadLine());
-
-            var nums = new int[n];
-            for (int i = 0; i < n; i++)
+            int factZero = 1;
+            int factOne = 1;
+            for (int i = 0; i < n - 1; i++)
             {
-
+                int factNext = factZero + factOne;
+                factZero = factOne;
+                factOne = factNext;
             }
-
+            Console.WriteLine(factOne);
         }
     }
 }
