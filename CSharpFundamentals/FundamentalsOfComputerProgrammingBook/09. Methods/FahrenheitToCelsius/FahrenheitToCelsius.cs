8db3b6187f1fc514da233025836584d763e311b9
valchev0 <georgi.v@mail.com>
2024-09-08 17:56:35 +0100

    Fahrenheit To Celsius

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/09. Methods/FahrenheitToCelsius/FahrenheitToCelsius.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FahrenheitToCelsius
+{
+    internal class FahrenheitToCelsius
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit = double.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Temperature in Celsius: {0}", Convertor(fahrenheit));
+        }
+
+        private static double Convertor(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+
+            return celsius;
+        }
+    }
+}
