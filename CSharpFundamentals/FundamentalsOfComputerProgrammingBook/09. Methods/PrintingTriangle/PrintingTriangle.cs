c84f434f4a119c3a9416635b9d06efb09a836d00
valchev0 <georgi.v@mail.com>
2024-09-08 17:20:48 +0100

    Printing Triangle

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/09. Methods/PrintingTriangle/PrintingTriangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PrintingTriangle
+{
+    internal class PrintingTriangle
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            TopPart(n);
+            BottomPart(n);
+        }
+
+        private static void BottomPart(int n)
+        {
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", j);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void TopPart(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
