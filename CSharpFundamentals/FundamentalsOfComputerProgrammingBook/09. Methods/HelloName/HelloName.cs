67335f8522fddfe84c7d9d4a4201a8a023fd2d20
valchev0 <georgi.v@mail.com>
2024-09-09 22:16:40 +0100

    Hello Name

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/09. Methods/HelloName/HelloName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloName
+{
+    internal class HelloName
+    {
+        static void Main(string[] args)
+        {
+            NamePrinter();
+        }
+
+        private static void NamePrinter()
+        {
+            Console.Write("Hi, what's your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
