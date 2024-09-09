b494e439c47469439a50d1712d2d05fdd7b37fa8
valchev0 <georgi.v@mail.com>
2024-09-09 14:42:22 +0100

    Modify Array

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/09. Methods/ModifyArray/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ArrayModifier
+{
+    internal class Program
+    {
+        static void Main()
+
+        {
+
+            int[] arrArg = new int[] { 1, 2, 3 };
+
+
+
+            Console.Write("Before ModifyArray() the argument is: ");
+
+            PrintArray(arrArg);
+
+
+
+            // Modifying the array's argument
+
+            ModifyArray(arrArg);
+
+
+
+            Console.Write("After ModifyArray() the argument is: ");
+
+            PrintArray(arrArg);
+
+        }
+        static void ModifyArray(int[] arrParam)
+
+        {
+
+            arrParam[0] = 5;
+
+
+
+            Console.Write("In ModifyArray() the param is: ");
+
+            PrintArray(arrParam);
+
+        }
+
+
+
+        static void PrintArray(int[] arrParam)
+
+        {
+
+            Console.Write("[");
+
+            int length = arrParam.Length;
+
+            if (length > 0)
+
+            {
+
+                Console.Write(arrParam[0].ToString());
+
+                for (int i = 1; i < length; i++)
+
+                {
+
+                    Console.Write(", {0}", arrParam[i]);
+
+                }
+
+            }
+
+            Console.WriteLine("]");
+
+        }
+    }
+}
