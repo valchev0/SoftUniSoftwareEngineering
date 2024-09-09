fcca6ad837568bdefba21e332e6260e990d37e4b
valchev0 <georgi.v@mail.com>
2024-09-09 22:41:39 +0100

    Digit To String

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/09. Methods/DigitToString/DigitToString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DigitToString
+{
+    internal class DigitToString
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(ConvertToString(n));
+        }
+        static string ConvertToString(int n)
+        {
+            int lastDigit = n % 10;
+            string result = string.Empty;
+
+            switch (lastDigit)
+            {
+                case 0:
+                    result = "Zero";
+                    break;
+                case 1:
+                    result = "One";
+                    break;
+                case 2:
+                    result = "Two";
+                    break;
+                case 3:
+                    result = "Three";
+                    break;
+                case 4:
+                    result = "Four";
+                    break;
+                case 5:
+                    result = "Five";
+                    break;
+                case 6:
+                    result = "Six";
+                    break;
+                case 7:
+                    result = "Seven";
+                    break;
+                case '8':
+                    result = "Eight";
+                    break;
+                case 9:
+                    result = "Nine";
+                    break;
+            }
+            return result;
+        }
+    }
+}
