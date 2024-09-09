4e1b5eeda190156b77a933f0e80a4838fb0031c3
valchev0 <georgi.v@mail.com>
2024-09-09 23:01:35 +0100

    Digit Contains in Array

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/09. Methods/DigitContainsInArray/DigitContainsInArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DigitContainsInArray
+{
+    internal class DigitContainsInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.Write("Choose a number: ");
+
+            Console.WriteLine("The number was found {0} times", ContainCounter(arr));
+
+        }
+        static int ContainCounter(int[] arr)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == n)
+                    counter++;
+            }
+            return counter;
+        }
+    }
+}
