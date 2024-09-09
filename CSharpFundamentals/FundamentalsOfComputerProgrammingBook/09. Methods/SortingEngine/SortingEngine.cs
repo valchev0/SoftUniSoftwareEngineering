4ec673e37ef08ae0668bb1aea3a5ccf04b4bab9b
valchev0 <georgi.v@mail.com>
2024-09-09 21:04:13 +0100

    Sorting Engine

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/09. Methods/SortingEngine/SortingEngine.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SortingEngine
+{
+    internal class SortingEngine
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Sort(10, 3, 5, -1, 0, 12, 8);
+
+            PrintNumbers(numbers);
+        }
+        static int[] Sort(params int[] numbers)
+
+        {
+
+            // The sorting logic:
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+
+            {
+
+                // Loop that is operating over the un-sorted part of
+
+                // the array
+
+                for (int j = i + 1; j < numbers.Length; j++)
+
+                {
+
+                    // Swapping the values
+
+                    if (numbers[i] > numbers[j])
+
+                    {
+
+                        int oldNum = numbers[i];
+
+                        numbers[i] = numbers[j];
+
+                        numbers[j] = oldNum;
+
+                    }
+
+                }
+
+            } // End of the sorting logic
+
+            return numbers;
+
+        }
+        static void PrintNumbers(params int[] numbers)
+
+        {
+
+            for (int i = 0; i < numbers.Length; i++)
+
+            {
+
+                Console.Write("{0}", numbers[i]);
+
+                if (i < (numbers.Length - 1))
+
+                {
+
+                    Console.Write(", ");
+
+                }
+
+            }
+
+        }
+    }
+    
+}
