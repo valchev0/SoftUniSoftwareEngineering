afd8d6a3e145eec7436f18aa8aed3fa71b1f5ac7
valchev0 <georgi.v@mail.com>
2024-09-09 22:21:21 +0100

    Max of Three Numbers

---
+++ CSharpFundamentals/FundamentalsOfComputerProgrammingBook/09. Methods/MaxOfThreeNumbers/MaxOfThreeNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MaxOfThreeNumbers
+{
+    internal class MaxOfThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            GetMax(a, b, c);
+        }
+
+        private static void GetMax(int a, int b, int c)
+        {
+            int result = Math.Max(a, Math.Max(b, c));
+            Console.WriteLine(result);
+        }
+    }
+}
