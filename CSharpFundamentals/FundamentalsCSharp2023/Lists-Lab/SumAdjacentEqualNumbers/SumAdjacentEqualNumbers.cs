4387f668153ad84c55bda33b3df8080e04a091ea
valchev0 <georgi.v@mail.com>
2024-09-22 05:06:07 +0100

    Sum Adjacent Equal Numbers

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Lists-Lab/SumAdjacentEqualNumbers/SumAdjacentEqualNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SumAdjacentEqualNumbers
+{
+    internal class SumAdjacentEqualNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = new List<double>();
+            string input = Console.ReadLine();
+            List<string> list = input.Split(' ').ToList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                numbers.Add(double.Parse(list[i]));
+            }
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                while (i < numbers.Count - 1)
+                {
+                    if (numbers[i] == numbers[i + 1])
+                    {
+                        numbers[i] = numbers[i] + numbers[i + 1];
+                        numbers.RemoveAt(i + 1);
+                        if (i > 0) // връщаме брояча с една позиция, защото като премахнем елемент, трябва да проверяваме и следващия
+                        {
+                            i--;
+                        }
+                    }
+                    else
+                    {
+                        i++;
+                    }
+
+                }
+            }
+        }
+    }
+}
