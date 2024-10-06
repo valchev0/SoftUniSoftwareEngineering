8b4bc4874abca44f32575b436d2de127383e3a1d
valchev0 <georgi.v@mail.com>
2024-10-06 13:43:19 +0100

    List Manipulation Basics

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Lists-Lab/ListManipulationBasics/ListManipulationBasics.cs
@@ -0,0 +1,37 @@
+﻿namespace ListManipulationBasics
+{
+    internal class ListManipulationBasics
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] commandParts = command.Split(' ');
+                string action = commandParts[0];
+                int number = int.Parse(commandParts[1]);
+                switch (action)
+                {
+                    case "Add":
+                        list.Add(number);
+                        break;
+                    case "Remove":
+                        list.Remove(number);
+                        break;
+                    case "RemoveAt":
+                        list.RemoveAt(number);
+                        break;
+                    case "Insert":
+                        int index = int.Parse(commandParts[2]);
+                        list.Insert(index, number);
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
