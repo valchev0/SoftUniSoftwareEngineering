db4580383521588e1d0b295da834e353bb165413
valchev0 <georgi.v@mail.com>
2024-10-06 12:18:16 +0100

    Merging Lists

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Lists-Lab/MergingLists/MergingLists.cs
@@ -0,0 +1,37 @@
+﻿namespace MergingLists
+{
+    internal class MergingLists
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> secondList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> results = new List<int>();
+
+            int n = Math.Min(firstList.Count, secondList.Count);
+
+            for (int i = 0; i < n; i++)
+            {
+                results.Add(firstList[i]);
+                results.Add(secondList[i]);
+            }
+
+            if (firstList.Count > secondList.Count)
+            {
+                results.AddRange(firstList.GetRange(n, firstList.Count - n));
+            }
+            else if (secondList.Count > firstList.Count)
+            {
+                results.AddRange(secondList.GetRange(n, secondList.Count - n));
+            }
+
+            Console.WriteLine(string.Join(" ", results));
+        }
+    }
+}
