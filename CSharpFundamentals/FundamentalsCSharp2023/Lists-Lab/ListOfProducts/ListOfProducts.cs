54da7c46e8659be4ab8417aa726e081da5fdf995
valchev0 <georgi.v@mail.com>
2024-10-06 12:41:47 +0100

    List of Products

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Lists-Lab/ListOfProducts/ListOfProducts.cs
@@ -0,0 +1,31 @@
+﻿namespace ListOfProducts
+{
+    internal class ListOfProducts
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> list = new List<string>();
+            AbsorbingMetod(n, list);
+            list.Sort();
+            PrintingMethod(list);
+        }
+        static void AbsorbingMetod(int n, List<string> list)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+        }
+        static void PrintingMethod(List<string> list)
+        {
+            int counter = 1;
+            foreach (string element in list)
+            {
+                Console.WriteLine("{0}.{1}", counter, element);
+                counter++;
+            }
+        }
+    }
+}
