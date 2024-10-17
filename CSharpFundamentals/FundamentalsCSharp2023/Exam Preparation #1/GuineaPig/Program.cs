e253efbb53d20ca39a58273f28cf667e428f5395
valchev0 <georgi.v@mail.com>
2024-10-17 07:36:36 +0100

    Guinea Pig

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Exam Preparation #1/GuineaPig/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace GuineaPig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal foodQuantity = decimal.Parse(Console.ReadLine());
+            decimal hayQuantity = decimal.Parse(Console.ReadLine());
+            decimal coverQuantity = decimal.Parse(Console.ReadLine());
+            decimal guineaWeight = decimal.Parse(Console.ReadLine());
+            int days = 30;
+
+            bool isLoopEnd = false;
+            decimal amountHay = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                if (!isLoopEnd && foodQuantity >= 0.31m)
+                {
+                    foodQuantity -= 0.3m;
+                }
+                else
+                {
+                    Console.WriteLine("Merry must go to the pet store!");
+                    isLoopEnd = true;
+                    break;
+                }
+
+                if (!isLoopEnd && (i % 2 == 0))
+                {
+                    if (!isLoopEnd)
+                    {
+                        amountHay = foodQuantity * 0.05m;
+
+                        if (hayQuantity >= amountHay)
+                        {
+                            hayQuantity -= amountHay;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Merry must go to the pet store!");
+                            isLoopEnd = true;
+                            break;  
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Merry must go to the pet store!");
+                        isLoopEnd = true;
+                        break;
+                    }
+                }
+                if (!isLoopEnd && (i % 3 == 0))
+                {
+                    decimal coverNeeded = guineaWeight / 3.0m;
+                    if (!isLoopEnd && (coverQuantity >= coverNeeded))
+                    {
+                        coverQuantity -= coverNeeded;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Merry must go to the pet store!");
+                        isLoopEnd = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!isLoopEnd)
+            {
+                Console.WriteLine("Everything is fine! Puppy is happy! Food: {0:0.00}, Hay: {1:0.00}, Cover: {2:0.00}.", foodQuantity, hayQuantity, coverQuantity);
+            }
+        }
+    }
+}
--- CSharpFundamentals/FundamentalsCSharp2023/List-Exercise/AppendArrays/Program.cs
+++ CSharpFundamentals/FundamentalsCSharp2023/List-Exercise/AppendArrays/Program.cs
@@ -1,32 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace AppendArrays
+﻿namespace AppendArrays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Четене на входните данни
             string input = Console.ReadLine();
-
-            // Разделяне на масивите по '|'
             string[] arrays = input.Split('|', StringSplitOptions.RemoveEmptyEntries);
-
-            // Създаване на нов списък за резултата
             List<int> result = new List<int>();
 
-            // Обработка на масивите отдясно наляво
             for (int i = arrays.Length - 1; i >= 0; i--)
             {
-                // Разделяне на стойностите в текущия масив и добавяне към резултата
                 result.AddRange(arrays[i]
                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                     .Select(int.Parse));
             }
 
-            // Извеждане на резултата
             Console.WriteLine(string.Join(" ", result));
         }
     }
