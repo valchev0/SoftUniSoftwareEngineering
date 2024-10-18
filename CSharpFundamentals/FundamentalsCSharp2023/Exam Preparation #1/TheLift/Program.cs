98a03755d8a6b83254e5f06809d04aaf210ab452
valchev0 <georgi.v@mail.com>
2024-10-18 07:05:45 +0100

    The Lift

---
+++ CSharpFundamentals/FundamentalsCSharp2023/Exam Preparation #1/TheLift/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+
+            int[] wagons = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            bool areFul = false;
+            int counter = 0;
+            int queue = 0;
+
+            for (int i = 1; i <= people; i++)
+            {
+                if (counter > wagons.Length - 1)
+                {
+                    queue = i;
+                    areFul = true;
+                    break;
+                }
+                wagons[counter]++;
+                if (wagons[counter] == 4)
+                {
+                    counter++;
+                }
+            }
+            if (areFul)
+            {
+                Console.WriteLine(($"There isn't enough space! {queue - 1} people in a queue!"));
+            }
+            else
+            {
+                Console.WriteLine($"The lift has empty spots!");
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+            
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsCSharp2023/Mid Exam Preparation/TheLift/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+
+            int[] wagons = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int counter = 0;
+
+            while (people > 0 && counter < wagons.Length)
+            {
+                int availableSpace = 4 - wagons[counter];
+                if (people >= availableSpace)
+                {
+                    wagons[counter] += availableSpace;
+                    people -= availableSpace;
+                }
+                else
+                {
+                    wagons[counter] += people;
+                    people = 0;
+                }
+
+                if (wagons[counter] == 4)
+                {
+                    counter++;
+                }
+            }
+
+            if (people > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {people} people in a queue!");
+            }
+            else if (wagons.Any(w => w < 4))
+            {
+                Console.WriteLine("The lift has empty spots!");
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
