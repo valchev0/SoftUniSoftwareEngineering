d1886a61d4be5d3dfcbccf452686298a5cdb86b1
valchev0 <georgi.v@mail.com>
2024-10-20 23:26:10 +0100

    Shoot for the Win

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Mid Exam Preparation/ShootForTheWin/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ 
+ */
+namespace ShootForTheWin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = string.Empty;
+            int temp = 0;
+            int shooter = 0;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                int target = int.Parse(command);
+
+                if (target > -1 && target < array.Length)
+                {
+                    if (array[target] != -1)
+                    {
+                        temp = array[target];
+                        array[target] = -1;
+                        shooter++;
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            if (array[i] != -1)
+                            {
+                                 if (array[i] > temp)
+                                 {
+                                     array[i] -= temp;
+                                 }
+                                 else
+                                 {
+                                     array[i] += temp;
+                                 }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.Write($"Shot targets: {shooter} -> ");
+            Console.Write(string.Join(" ", array));
+        }
+    }
+}
