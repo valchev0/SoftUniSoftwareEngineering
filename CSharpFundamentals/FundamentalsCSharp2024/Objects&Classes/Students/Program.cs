a7b6bfca148cb34e4194433a51f8ea6df3e8f38c
valchev0 <georgi.v@mail.com>
2024-11-02 14:51:36 +0000

    Students 2.0

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Objects&Classes/Students/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            List<Student> students = new();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] studentProps = input.Split();
+
+                Student student = new()
+                {
+                    FirstName = studentProps[0],
+                    LastName = studentProps[1],
+                    Age = int.Parse(studentProps[2]),
+                    HomeTown = studentProps[3]
+                };
+
+                students.Add(student);
+            }
+            string city = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == city)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string HomeTown { get; set; }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsCSharp2024/Objects&Classes/Students2.0/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Students2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            List<Student> students = new List<Student>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] data = input.Split();
+
+                Student existingStudent = students.FirstOrDefault(s => s.FirstName == data[0] && s.LastName == data[1]);
+
+                if (existingStudent != null)
+                {
+                    // Update the existing student's details
+                    existingStudent.Age = int.Parse(data[2]);
+                    existingStudent.HomeTown = data[3];
+                }
+                else
+                {
+                    // Add a new student if no match is found
+                    Student student = new()
+                    {
+                        FirstName = data[0],
+                        LastName = data[1],
+                        Age = int.Parse(data[2]),
+                        HomeTown = data[3]
+                    };
+                    students.Add(student);
+                }
+            }
+
+            string city = Console.ReadLine();
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == city)
+                {
+                    Console.WriteLine("{0} {1} is {2} years old.", student.FirstName, student.LastName, student.Age);
+                }
+            }
+        }
+    }
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
