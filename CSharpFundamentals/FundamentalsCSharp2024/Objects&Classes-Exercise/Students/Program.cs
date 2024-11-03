dd4020ebfc42ab9188751a4c59a8bcdcd1044f4d
valchev0 <georgi.v@mail.com>
2024-11-03 06:35:13 +0000

    Students

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Objects&Classes-Exercise/Students/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(':',' ');
+                Student student = new()
+                {
+                    FirstName = tokens[0],
+                    LastName = tokens[1],
+                    Grade = double.Parse(tokens[2])
+                };
+                students.Add(student);
+            }
+            List<Student> orderedStudents = students.OrderByDescending(student => student.Grade).ToList();
+            foreach (Student student in orderedStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+            }
+        }
+    }
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+    }
+}
