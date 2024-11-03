af9f9d6b14581e7a1014906f8cf8394d2b58c717
valchev0 <georgi.v@mail.com>
2024-11-03 06:03:12 +0000

    Articles

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Objects&Classes-Exercise/Articles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Channels;
+
+namespace Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Article article = new Article();
+
+            string[] data = Console.ReadLine().Split(',');
+            article.Title = data[0];
+            article.Content = data[1];
+            article.Author = data[2];
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] tokens = input.Split(":");
+                switch (tokens[0])
+                {
+                    case "Rename":
+                        article.Title = tokens[1];
+                        break;
+                    case "Edit":
+                        article.Content = tokens[1];
+                        break;
+                    case "ChangeAuthor":
+                        article.Author = tokens[1];
+                        break;
+                }
+            }
+            Console.WriteLine($"{article.Title} -{article.Content}:{article.Author}");
+        }
+    }
+    class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+}
+
