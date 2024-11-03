0227776a6e211937c54ed855fb5367ddf410c458
valchev0 <georgi.v@mail.com>
2024-11-03 06:18:13 +0000

    Articles 2.0

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Objects&Classes-Exercise/Articles2.0/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Articles2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Article> articles = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] tokens = input.Split(", ");
+                Article article = new()
+                {
+                    Title = tokens[0],
+                    Content = tokens[1],
+                    Author = tokens[2]
+                };
+                articles.Add(article);
+            }
+            foreach (var article in articles)
+            {
+                Console.WriteLine($"{article.Title} - {article.Content}: {article.Author}");
+            }
+        }
+    }
+    class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+}
+
