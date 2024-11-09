d1f4f06f9b66a286276d152589899acec8d7a11b
valchev0 <georgi.v@mail.com>
2024-11-09 23:47:37 +0000

    Count Real Numbers

---
+++ CSharpFundamentals/FundamentalsCSharp2024/AssociativeArrays/CountRealNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> map = new SortedDictionary<double, int>();
+
+            foreach (var element in array)
+            {
+                if (map.ContainsKey(element))
+                {
+                    map[element]++;
+                }
+                else
+                {
+                    map.Add(element, 1);
+                }
+            }
+            foreach (var element in map)
+            {
+                Console.WriteLine($"{element.Key} -> {element.Value}");
+            }
+        }
+    }
+}
