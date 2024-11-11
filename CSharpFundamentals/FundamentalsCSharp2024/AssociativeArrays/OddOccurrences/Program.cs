4ef5106bf6847b7bab442e1707c5bec24c50c198
valchev0 <georgi.v@mail.com>
2024-11-11 00:38:33 +0000

    Odd Occurrences

---
+++ CSharpFundamentals/FundamentalsCSharp2024/AssociativeArrays/OddOccurrences/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordsInLowerCase = word.ToLower();
+                if (counts.ContainsKey(wordsInLowerCase))
+                {
+                    counts[wordsInLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordsInLowerCase, 1);
+                }
+            }
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
