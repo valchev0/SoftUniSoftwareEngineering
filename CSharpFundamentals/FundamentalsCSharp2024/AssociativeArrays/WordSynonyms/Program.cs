3887610c6b4f11fc186dc2e740164e91a13cce6a
valchev0 <georgi.v@mail.com>
2024-11-16 17:51:14 +0000

    Word Synonyms

---
+++ CSharpFundamentals/FundamentalsCSharp2024/AssociativeArrays/WordSynonyms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> synonyms = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                string key = Console.ReadLine();
+                string value = Console.ReadLine();
+                if (!synonyms.ContainsKey(key))
+                {
+                    synonyms[key] = new List<string>();
+                }
+                synonyms[key].Add(value);
+            }
+
+            foreach (var synonym in synonyms)
+            {
+                Console.WriteLine($"{synonym.Key} - {string.Join(", ", synonym.Value)}");
+            }
+        }
+    }
+}
