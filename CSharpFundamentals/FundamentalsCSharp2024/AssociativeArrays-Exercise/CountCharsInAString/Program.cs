2c44e77db41a82c7b72e5646596da8934c3e44ca
valchev0 <georgi.v@mail.com>
2024-11-17 08:00:32 +0000

    Count Chars in a String

---
+++ CSharpFundamentals/FundamentalsCSharp2024/AssociativeArrays-Exercise/CountCharsInAString/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArray = Console.ReadLine()
+                .Where(x => !char.IsWhiteSpace(x))
+                .ToArray();
+
+            Dictionary<char, int> chars = new();
+
+            foreach (char chr in charArray)
+            {
+                if (!chars.ContainsKey(chr))
+                {
+                    chars.Add(chr, 0);
+                }
+                chars[chr]++;
+            }
+            foreach (var chr in chars)
+            {
+                Console.WriteLine($"{chr.Key} -> {chr.Value}");
+            }
+        }
+    }
+}
