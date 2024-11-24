9d2e3b104e8fd204eeac253a299261d53e0e2df5
valchev0 <georgi.v@mail.com>
2024-11-24 19:58:55 +0000

    Company Users

---
+++ CSharpFundamentals/FundamentalsCSharp2024/AssociativeArrays-Exercise/CompanyUsers/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Company> companies = new();
+            string input;
+            
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] tokens = input.Split(" -> ");
+                string name = tokens[0];
+                string id = tokens[1];
+
+                if (!companies.ContainsKey(name))
+                {
+                    companies[name] = new Company()
+                    {
+                        CompanyName = name,
+                        CompanyIds = new List<string>() { id }
+                    };
+                }
+                else if (!companies[name].CompanyIds.Contains(id))
+                {
+                    companies[name].CompanyIds.Add(id);
+                }
+            }
+            foreach (var company in companies)
+            {
+                Console.WriteLine(company.Key);
+                foreach (var id in company.Value.CompanyIds)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+    public class Company
+    {
+        public string CompanyName { get; set; }
+
+        public List<string> CompanyIds { get; set; }
+    }
+}
