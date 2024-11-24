44ea11a87642ff2da53ba8def599e9e606beed00
valchev0 <georgi.v@mail.com>
2024-11-24 18:33:10 +0000

    Student Academy

---
+++ CSharpFundamentals/FundamentalsCSharp2024/AssociativeArrays-Exercise/StudentAcademy/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Student> students = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                decimal grade = decimal.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(name))
+                {
+                    students[name] = new Student()
+                    {
+                        Name = name,
+                        Grade = new List<decimal> { grade }
+                    };
+                }
+                else
+                {
+                    students[name].Grade.Add(grade);
+                }
+            }
+            foreach ( var student in students)
+            {
+                if (student.Value.Grade.Average() >= 4.5m)
+                {
+                    Console.WriteLine($"{student.Key} -> {student.Value.Grade.Average():F2}");
+                }
+            }
+        }
+    }
+    public class  Student
+    {
+        public string Name { get; set; }
+        public List<decimal> Grade { get; set; }
+    }
+}
