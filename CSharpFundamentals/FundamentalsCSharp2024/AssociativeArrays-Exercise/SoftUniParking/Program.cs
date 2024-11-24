000faa81b728c2f87e5c348b8b4a69520d9681df
valchev0 <georgi.v@mail.com>
2024-11-24 15:34:57 +0000

    SoftUni Parking

---
+++ CSharpFundamentals/FundamentalsCSharp2024/AssociativeArrays-Exercise/SoftUniParking/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace SoftUniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Parking> registers = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string userName = tokens[1];
+
+                switch (tokens[0])
+                {
+                    case "register":
+                        string licensePlateNumber = tokens[2];
+                        if (!registers.ContainsKey(tokens[1]))
+                        {
+                            registers[userName] = new Parking()
+                            {
+                                Name = userName,
+                                PlateNumber = licensePlateNumber
+                            };
+                            Console.WriteLine($"{userName} registered {licensePlateNumber} successfully");
+                            break;
+                        }
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                        continue;
+                    case "unregister":
+                        if (!registers.ContainsKey(tokens[1]))
+                        {
+                            Console.WriteLine($"ERROR: user {userName} not found");
+                            continue;
+                        }
+                        registers.Remove(userName);
+                        Console.WriteLine($"{userName} unregistered successfully");
+                        break;
+                }
+            }
+            foreach (var register in registers)
+            {
+                Console.WriteLine($"{register.Key} => {register.Value.PlateNumber}");
+            }
+        }
+    }
+    public class Parking
+    {
+        public string Name { get; set; }
+
+        public string PlateNumber { get; set; }
+    }
+}
