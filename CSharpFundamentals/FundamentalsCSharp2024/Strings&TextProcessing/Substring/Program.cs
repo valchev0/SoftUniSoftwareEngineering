edf8b6a2cdcdc17d9a0894c0fe4d6b1a9f4d2ff6
valchev0 <georgi.v@mail.com>
2024-11-30 15:06:49 +0000

    Substring

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing/Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string remover = Console.ReadLine();
+
+            string chars = Console.ReadLine();
+
+            while (chars.Contains(remover))
+            {
+                chars = chars.Replace(remover, "");
+            }
+            string newString = chars;
+            Console.WriteLine(newString);
+        }
+    }
+}
