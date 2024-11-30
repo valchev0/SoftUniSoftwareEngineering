0c3ec66ea6f4437136a73cd7ac388fc1784ec724
valchev0 <georgi.v@mail.com>
2024-11-30 14:52:29 +0000

    Repeat Strings

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing/RepeatStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var word in array)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    sb.Append(word);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
