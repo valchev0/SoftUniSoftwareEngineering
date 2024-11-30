1a73b48c2da3e576c6505867d8d9cd317ee1ce6a
valchev0 <georgi.v@mail.com>
2024-11-30 14:48:22 +0000

    Reverse Strings

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing/ReverseStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = string.Empty;
+
+            while ((word = Console.ReadLine()) != "end")
+            {
+                char[] chrArr = word.Reverse().ToArray();
+
+                Console.WriteLine($"{word} = {string.Join("", chrArr)}");
+            }
+        }
+    }
+}
