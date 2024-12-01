abf27d6ce5e0cdcf4b067b49b8a4c61b0542caf6
valchev0 <georgi.v@mail.com>
2024-12-01 12:16:35 +0000

    Hogwarts

---
+++ CSharpFundamentals/FundamentalsCSharp2024/FinalExam/Hogwarts/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Hogwarts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Abracadabra")
+            {
+                string[] arr = command.Split();
+                switch (arr[0])
+                {
+                    case "Abjuration":
+                        input = input.ToUpper();
+                        Console.WriteLine(input);
+                        break;
+                    case "Necromancy":
+                        input = input.ToLower();
+                        Console.WriteLine(input);
+                        break;
+                    case "Illusion":
+                        if (int.Parse(arr[1]) >= 0 && int.Parse(arr[1]) < input.Length)
+                        {
+                            char[] chars = input.ToCharArray();
+                            chars[int.Parse(arr[1])] = char.Parse(arr[2]);
+                            input = new string(chars);
+                            Console.WriteLine("Done!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The spell was too weak.");
+                        }
+                        break;
+                    case "Divination":
+                        if (input.Contains(arr[1]))
+                        {
+                            string temp = input.Replace(arr[1], arr[2]);
+                            input = temp;
+                            Console.WriteLine(input);
+                        }
+                        else
+                        {
+                            Console.WriteLine("The spell was too weak.");
+                        }
+                        break;
+                    case "Alteration":
+                        if (input.Contains(arr[1]))
+                        {
+                            input = input.Replace(arr[1], string.Empty);
+                            Console.WriteLine(input);
+                        }
+                        else
+                        {
+                            Console.WriteLine("The spell did not work!");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("The spell did not work!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing/TextFilter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+        }
+    }
+}
