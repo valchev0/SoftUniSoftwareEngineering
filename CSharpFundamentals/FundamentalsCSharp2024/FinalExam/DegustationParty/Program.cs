8ffca2f59f4f9bac70e30edf4e0a9b1ad8af7fea
valchev0 <georgi.v@mail.com>
2024-12-01 12:19:59 +0000

    Degustation Party

---
+++ CSharpFundamentals/FundamentalsCSharp2024/FinalExam/DegustationParty/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace DegustationParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Degustation> degustations = new();
+            string command;
+            int unliked = 0;
+
+            while ((command = Console.ReadLine()) != "Stop")
+            {
+                string[] arr = command.Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+                if (arr[0] == "Like")
+                {
+                    if (!degustations.ContainsKey(arr[1]))
+                    {
+                        degustations.Add(arr[1], new Degustation()
+                        {
+                            Name = arr[1],
+                            FeedBack = arr[0],
+                            Meals = new List<string>() { arr[2] }
+                        });
+                    }
+                    else if (!degustations[arr[1]].Meals.Contains(arr[2]))
+                    {
+                        degustations[arr[1]].Meals.Add(arr[2]);
+                    }
+                }
+                else if (arr[0] == "Dislike")
+                {
+                    if (degustations.ContainsKey(arr[1]))
+                    {
+                        if (degustations[arr[1]].Meals.Contains(arr[2]))
+                        {
+                            degustations[arr[1]].Meals.Remove(arr[2]);
+                            Console.WriteLine($"{arr[1]} doesn't like the {arr[2]}.");
+                            unliked++;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{arr[1]} doesn't have the {arr[2]} in his/her collection.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{arr[1]} is not at the party.");
+                    }
+                }
+            }
+
+            foreach (var guest in degustations)
+            {
+                Console.WriteLine($"{guest.Key}: {string.Join(", ", guest.Value.Meals)}");
+            }
+            Console.WriteLine($"Unliked meals: {unliked}");
+        }
+    }
+
+    public class Degustation
+    {
+        public string Name { get; set; }
+
+        public string FeedBack { get; set; }
+
+        public List<string> Meals { get; set; }
+    }
+}
