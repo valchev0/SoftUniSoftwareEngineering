24158f013ed547cce079d0cda4b03e8db5ce08bd
Georgi Valchev <123665151+valchev0@users.noreply.github.com>
2025-02-01 12:47:47 +0000

    Valid Usernames

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing-Ex/Strings&TextProcessing-Ex/ValidUsernames/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            List <string> validNames = new();
+
+            foreach (string username in usernames)
+            {
+                bool isValidName = username.All(character => char.IsLetterOrDigit(character) ||
+                                                             character == '_' ||
+                                                             character == '-');
+                if (username.Length >= 3
+                    && username.Length <= 16
+                    && isValidName)
+                {
+                    if (!validNames.Contains(username))
+                    {
+                        validNames.Add(username);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, validNames));
+        }
+    }
+}
