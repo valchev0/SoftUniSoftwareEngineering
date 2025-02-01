adf04dbb838f58c1f9a9b4f8312318039980865f
Georgi Valchev <123665151+valchev0@users.noreply.github.com>
2025-02-01 13:38:01 +0000

    Character Multiplier

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing-Ex/Strings&TextProcessing-Ex/CharacterMultiplier/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split();
+            string first = strings[0];
+            string second = strings[1];
+
+            if (first.Length < second.Length)
+            {
+                var temp = first;
+                first = second;
+                second = temp;
+            }
+
+            Console.WriteLine(Sum(first, second));
+        }
+
+        static int Sum(string first, string second)
+        {
+            int totalSum = 0;
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (i < second.Length)
+                {
+                    totalSum += first[i] * second[i];
+                }
+                else
+                {
+                    totalSum += first[i];
+                }
+            }
+
+            return totalSum;
+        }
+    }
+}
