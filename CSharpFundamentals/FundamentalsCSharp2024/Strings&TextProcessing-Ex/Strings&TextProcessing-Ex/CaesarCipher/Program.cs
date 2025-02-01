c9d6cb5c0387f47e44673dc0c6cad50027ee1af4
Georgi Valchev <123665151+valchev0@users.noreply.github.com>
2025-02-01 16:08:35 +0000

    Caesar Cipher

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing-Ex/Strings&TextProcessing-Ex/CaesarCipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArray = Console.ReadLine().ToCharArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char ch in charArray)
+            {
+                char shift = (char)(ch + 3);
+                sb.Append(shift);
+            }
+            Console.WriteLine(string.Join("", sb));
+        }
+    }
+}
