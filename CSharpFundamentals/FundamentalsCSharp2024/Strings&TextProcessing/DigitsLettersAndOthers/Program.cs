71d5c6df10e5684bfdb028fdd59c9969d84f68f5
Georgi Valchev <123665151+valchev0@users.noreply.github.com>
2025-02-01 06:53:06 +0000

    Digits, Letters and Other

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing/DigitsLettersAndOthers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Linq;
+
+namespace DigitsLettersAndOthers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalString = Console.ReadLine();
+
+            string digits = new string(originalString.Where(char.IsDigit).ToArray());
+
+            Console.WriteLine(digits);
+
+            string letters = new string(originalString.Where(char.IsLetter).ToArray());
+
+            Console.WriteLine(letters);
+
+            string symbols = new string(originalString.Where(ch => !char.IsLetterOrDigit(ch)).ToArray());
+
+            Console.WriteLine(symbols);
+        }
+    }
+}
