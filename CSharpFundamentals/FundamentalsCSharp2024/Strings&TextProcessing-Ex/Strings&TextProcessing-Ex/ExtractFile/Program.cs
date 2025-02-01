875641d3ad8f6e83d99a0d13bafd57cf23e2fda9
Georgi Valchev <123665151+valchev0@users.noreply.github.com>
2025-02-01 14:13:54 +0000

    Extract File

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing-Ex/Strings&TextProcessing-Ex/ExtractFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            string extension = path.Substring(path.LastIndexOf('.') + 1);
+
+            StringBuilder fileName = new StringBuilder(path.Substring(path.LastIndexOf('\\') + 1).Replace(extension, ""));
+
+            Console.WriteLine($"File name: {fileName.Replace(".", "")}");
+
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
