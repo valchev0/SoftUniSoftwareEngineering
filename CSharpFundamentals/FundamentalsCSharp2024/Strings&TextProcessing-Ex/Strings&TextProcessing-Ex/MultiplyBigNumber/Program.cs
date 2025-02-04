e4b9958bb1757bc1e262f0bc2d5973dbbc310cec
Georgi Valchev <123665151+valchev0@users.noreply.github.com>
2025-02-04 22:14:18 +0000

    Replace Repeating Chars

---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing-Ex/Strings&TextProcessing-Ex/MultiplyBigNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger first = BigInteger.Parse(Console.ReadLine());
+
+            int second = int.Parse(Console.ReadLine());
+
+            first *= second;
+
+            Console.WriteLine(first);
+        }
+    }
+}
---
+++ CSharpFundamentals/FundamentalsCSharp2024/Strings&TextProcessing-Ex/Strings&TextProcessing-Ex/ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] chars = input.ToCharArray();
+
+            char tempChar = '\0';
+
+            foreach (char ch in chars)
+            {
+                if (tempChar != ch)
+                {
+                    Console.Write(ch);
+                }
+                tempChar = ch;
+            }
+        }
+    }
+}
