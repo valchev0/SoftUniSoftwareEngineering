43c3cae3e22dfbf682b54f21bd8c48a22248d02d
Georgi Valchev <123665151+valchev0@users.noreply.github.com>
2025-02-15 06:18:41 +0000

    Match Phone Number

---
+++ CSharpFundamentals/FundamentalsCSharp2024/RegularExpressions-Lab/MatchPhoneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359( |-)2\1\d{3}\1\d{4}(?!\d)";
+
+            string input = Console.ReadLine();
+
+            var phoneMatches = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", phoneMatches.Select(x => x.Groups[0])));
+        }
+    }
+}
